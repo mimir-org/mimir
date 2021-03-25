48a052930c4fc1e991787362a961466edc661b2b
Landro <hml@bouvet.no>
2021-03-25 15:17:58 +0100

    Update padding and size column and input

--- src/client/src/components/modules/inspectorModule/fragments/styled/FragmentDataWrapper.tsx
+++ src/client/src/components/modules/inspectorModule/fragments/styled/FragmentDataWrapper.tsx
@@ -4,9 +4,8 @@ const FragmentDataWrapper = styled.div`
   background-color: #f2f2f2;
   color: #000;
   min-width: 25%;
-  height: 100vh;
   position: absolute;
-  padding: 10px 10px 10px 2px;
+  padding: 10px 10px 10px 0px;
   font-family: roboto;
   margin-top: 0px;
 `;
--- src/client/src/components/modules/inspectorModule/fragments/styled/StyledColumn.tsx
+++ src/client/src/components/modules/inspectorModule/fragments/styled/StyledColumn.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 const StyledColumn = styled.div`
-  padding: 10px;
+  padding-top: 15px;
+  padding-left: 15px;
   min-width: 200px;
 `;
 
