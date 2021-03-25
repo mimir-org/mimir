20dd2da44dd8fb83aa686841ca800fcb210622f3
Landro <hml@bouvet.no>
2021-03-25 19:09:52 +0100

    Remove unused css

--- src/client/src/components/modules/inspectorModule/styled/StyledInspectorComponent.tsx
+++ src/client/src/components/modules/inspectorModule/styled/StyledInspectorComponent.tsx
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 const StyledInspectorComponent = styled.div`
-  margin-bottom: 1px;
-  bottom: 0;
-  left: 20%;
-  right: 20%;
   border: 0px solid #000;
   height: ${(props: { stop: string }) => props.stop}px;
   background-color: #f2f2f2;
