91b0623596d749f35cfb981215529da575c7406a
mrlandro <helge.landro@bouvet.no>
2021-03-27 14:06:49 +0100

    Button styling

--- src/client/src/assets/buttons/ToggleInspectorButton.tsx
+++ src/client/src/assets/buttons/ToggleInspectorButton.tsx
@@ -3,16 +3,22 @@ import styled from "styled-components";
 const ToggleInspectorButton = styled.div`
   display: inline;
   margin-top: 8px;
+  margin-right: 10px;
+  float: right;
   border-left: 17px solid transparent;
   border-right: 17px solid transparent;
   border-top: ${(props: { visible: boolean }) =>
     props.visible ? "17px solid #007079" : "0px"};
   border-bottom: ${(props: { visible: boolean }) =>
     props.visible ? "0px" : "17px solid #007079"};
-  border-top-right-radius: 5px;
-  border-top-left-radius: 5px;
-  float: right;
-  margin-right: 10px;
+  border-top-right-radius: ${(props: { visible: boolean }) =>
+    props.visible ? "5px" : "0px"};
+  border-top-left-radius: ${(props: { visible: boolean }) =>
+    props.visible ? "5px" : "0px"};
+  border-bottom-right-radius: ${(props: { visible: boolean }) =>
+    props.visible ? "0px" : "5px"};
+  border-bottom-left-radius: ${(props: { visible: boolean }) =>
+    props.visible ? "0px" : "5px"};
 
   &: hover {
     cursor: pointer;
