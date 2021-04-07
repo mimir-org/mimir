af06e39e5a68bc721708a51f77aa2631489d03c3
mrlandro <helge.landro@bouvet.no>
2021-04-07 09:37:46 +0200

    Add cursor pointer

--- src/client/src/components/modules/explorerModule/facilityComponent/FacilityComponent.tsx
+++ src/client/src/components/modules/explorerModule/facilityComponent/FacilityComponent.tsx
@@ -7,7 +7,7 @@ import { RootState } from "../../../../redux/store";
 
 export const FacilityComponent = () => {
   const aspects = useSelector<RootState>(
-    (state) => state.projectState.project?.nodes
+    (state) => state.projectState.project.nodes
   );
 
   return (
@@ -16,9 +16,9 @@ export const FacilityComponent = () => {
         <CheckboxComponent id="1" inputLabel="Facility" />
       </FacilityHeader>
       <AspectWrapper>
-        <AspectComponent key={null} id={null} name={aspects[0].name} />
-        <AspectComponent key={null} id={null} name={aspects[1].name} />
-        <AspectComponent key={null} id={null} name={aspects[2].name} />
+        <AspectComponent key={null} id={aspects[0].id} name={aspects[0].name} />
+        <AspectComponent key={null} id={aspects[1].id} name={aspects[1].name} />
+        <AspectComponent key={null} id={aspects[2].id} name={aspects[2].name} />
         {/* {aspect.map(function (a, index) {
           return (
             <AspectComponent
