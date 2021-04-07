d0dd38da97813c4d1af02e163f789eb8ac06a6be
mrlandro <helge.landro@bouvet.no>
2021-04-07 12:01:47 +0200

    Explorer view controls treeView

--- src/client/src/components/flow/nodes/Aspect.tsx
+++ src/client/src/components/flow/nodes/Aspect.tsx
@@ -1,12 +1,26 @@
 import { memo, FC } from "react";
 import { NodeProps, Handle } from "react-flow-renderer";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
 import { GetAspectIcon } from "../helpers/";
 import { AspectWrapper } from "../styled/";
 import { processType } from "../utils";
 
 const Aspect: FC<NodeProps> = ({ data }) => {
+  const nodes: any = useSelector<RootState>(
+    (state) => state.projectState.project.nodes
+  );
+
+  let node: any = true;
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i].id === data.id) {
+      node = nodes[i];
+    }
+  }
+  const isVisible = node.isVisible;
+
   return (
-    <>
+    <AspectWrapper visible={isVisible}>
       {data.connectors &&
         data.connectors.map((connector) => {
           const [typeHandler, positionHandler] = processType(connector);
@@ -19,11 +33,10 @@ const Aspect: FC<NodeProps> = ({ data }) => {
             />
           );
         })}
-      <AspectWrapper visible={true}>
-        {GetAspectIcon(data.icon)}
-        <div>{data.label ?? data.name}</div>
-      </AspectWrapper>
-    </>
+
+      {GetAspectIcon(data.icon)}
+      <div>{data.label ?? data.name}</div>
+    </AspectWrapper>
   );
 };
 
