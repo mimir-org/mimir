ff8826e5cefbe886e53d60ba18aef60e13cea5dc
arisasel <arisa.seljestokken@bouvet.no>
2021-04-13 13:28:42 +0200

    added hover effect on options

--- src/client/src/components/modules/projectModule/optionsComponent/OptionsComponent.tsx
+++ src/client/src/components/modules/projectModule/optionsComponent/OptionsComponent.tsx
@@ -1,40 +1,58 @@
+import { useState } from "react";
 import {
   NewProjectIcon,
   OpenProjectIcon,
   RightArrowIcon,
+  WhiteRightArrowIcon,
 } from "../../../../assets";
 import textResources from "../../../../textResources";
 
 export const OptionsComponent = () => {
+  const [arrowIconImgSrc, setArrowIconImgSrc] = useState(RightArrowIcon);
+  const [secondArrowIconImgSrc, setSecondArrowIconImgSrc] = useState(
+    RightArrowIcon
+  );
   return (
     <div className="options_component_container">
       <div className="options_content">
         <p className="options_header">{textResources.Project_heading}</p>
-        <div className="option_container">
+        <div
+          className="option_container"
+          onMouseOver={() => {
+            setArrowIconImgSrc(WhiteRightArrowIcon);
+          }}
+          onMouseOut={() => {
+            setArrowIconImgSrc(RightArrowIcon);
+          }}
+        >
           <img
             className="option_icon"
             src={NewProjectIcon}
             alt="new-project-icon"
-          ></img>
+          />
           <p className="option_text">{textResources.Project_new_project}</p>
-          <img
-            className="arrow_icon"
-            src={RightArrowIcon}
-            alt="arrow-icon"
-          ></img>
+          <img className="arrow_icon" src={arrowIconImgSrc} alt="arrow-icon" />
         </div>
-        <div className="option_container">
+        <div
+          className="option_container"
+          onMouseOver={() => {
+            setSecondArrowIconImgSrc(WhiteRightArrowIcon);
+          }}
+          onMouseOut={() => {
+            setSecondArrowIconImgSrc(RightArrowIcon);
+          }}
+        >
           <img
             className="option_icon"
             src={OpenProjectIcon}
             alt="open-project-icon"
-          ></img>
+          />
           <p className="option_text">{textResources.Project_open_project}</p>
           <img
             className="arrow_icon"
-            src={RightArrowIcon}
+            src={secondArrowIconImgSrc}
             alt="arrow-icon"
-          ></img>
+          />
         </div>
       </div>
     </div>
