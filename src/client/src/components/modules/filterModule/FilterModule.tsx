aa627e18ce96546cde2372d014043bccdddcded9
mrlandro <helge.landro@bouvet.no>
2021-04-15 11:32:47 +0200

    Add FilterModule

--- src/client/src/components/modules/filterModule/FilterModule.tsx
+++ src/client/src/components/modules/filterModule/FilterModule.tsx
@@ -1,3 +1,10 @@
-const FilterModule = () => {};
+import textResources from "../../../textResources";
+import VisualFilterWrapper from "./styled/VisualFilterWrapper";
+
+const FilterModule = () => {
+  return (
+    <VisualFilterWrapper>{textResources.Filter_Types}</VisualFilterWrapper>
+  );
+};
 
 export default FilterModule;
---
+++ src/client/src/components/modules/filterModule/styled/VisualFilterWrapper.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const VisualFilterWrapper = styled.div`
+  background: #e5e5e5;
+  width: 334px;
+  height: 650px;
+  border: 2px solid #007079;
+  font-size: 18px;
+  font-family: roboto;
+  color: #007079;
+  padding: 10px;
+`;
+
+export default VisualFilterWrapper;
