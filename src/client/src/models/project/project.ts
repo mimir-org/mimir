6c185fa9531db0b12efc5e34f7d65daf9d100516
Reidar Liabø <RLIAB@equinor.com>
2021-04-19 10:46:56 +0200

    Attributes type and input type

--- src/client/src/models/project/project.ts
+++ src/client/src/models/project/project.ts
@@ -1,101 +1,119 @@
+export type AttributeInputType = keyof typeof ATTRIBUTE_INPUT_TYPE;
+
+export const ATTRIBUTE_INPUT_TYPE = {
+    TEXT: "Text",
+    LONG_TEXT: "LongText",
+    DROPDOWN: "Dropdown",
+    DATE: "Date"
+};
+
+export type AttributeType = keyof typeof ATTRIBUTE_TYPE;
+
+export const ATTRIBUTE_TYPE = {
+    ADMIN_INFO: "AdminInfo",
+    TECH_INFO: "TechInfo",
+    RELATIONS: "Relations"
+};
+
 export type EdgeType = keyof typeof EDGE_TYPE;
 
 export const EDGE_TYPE = {
-  DEFAULT: "DefaultEdgeType",
+    DEFAULT: "DefaultEdgeType",
 };
 
 export type LineEdgeType = keyof typeof LINE_EDGE_TYPE;
 
 export const LINE_EDGE_TYPE = {
-  STEP: "SmoothStepPath",
-  BEZIER: "BezierPath",
+    STEP: "SmoothStepPath",
+    BEZIER: "BezierPath",
 };
 
 export type NodeType = keyof typeof NODE_TYPE;
 
 export const NODE_TYPE = {
-  ASPECT_FUNCTION: "AspectFunction",
-  ASPECT_PRODUCT: "AspectProduct",
-  ASPECT_LOCATION: "AspectLocation",
-  FUNCTION: "Function",
-  PRODUCT: "Product",
-  LOCATION: "Location",
+    ASPECT_FUNCTION: "AspectFunction",
+    ASPECT_PRODUCT: "AspectProduct",
+    ASPECT_LOCATION: "AspectLocation",
+    FUNCTION: "Function",
+    PRODUCT: "Product",
+    LOCATION: "Location",
 };
 
 export type IconType = keyof typeof ICON_TYPE;
 
 export const ICON_TYPE = {
-  FUNCTION_ICON: "FunctionIcon",
-  LOCATION_ICON: "LocationIcon",
-  PRODUCT_ICON: "ProductIcon",
+    FUNCTION_ICON: "FunctionIcon",
+    LOCATION_ICON: "LocationIcon",
+    PRODUCT_ICON: "ProductIcon",
 };
 
 export type ConnectorType = keyof typeof CONNECTOR_TYPE;
 
 export const CONNECTOR_TYPE = {
-  RELATION_INPUT: "RelationInput",
-  RELATION_OUTPUT: "RelationOutput",
-  TRANSPORT_INPUT: "TransportInput",
-  TRANSPORT_OUTPUT: "TransportOutput",
-  PARTOF_INPUT: "PartofInput",
-  PARTOF_OUTPUT: "PartofOutput",
+    RELATION_INPUT: "RelationInput",
+    RELATION_OUTPUT: "RelationOutput",
+    TRANSPORT_INPUT: "TransportInput",
+    TRANSPORT_OUTPUT: "TransportOutput",
+    PARTOF_INPUT: "PartofInput",
+    PARTOF_OUTPUT: "PartofOutput",
 };
 export interface Project {
-  id: string;
-  name: string;
-  description: string;
-  nodes: Node[];
-  edges: Edge[];
+    id: string;
+    name: string;
+    description: string;
+    nodes: Node[];
+    edges: Edge[];
 }
 export interface Connector {
-  id: string;
-  name: string;
-  type: ConnectorType;
+    id: string;
+    name: string;
+    type: ConnectorType;
 }
 
 export interface Edge {
-  id: string;
-  fromConnector: string;
-  toConnector: string;
-  fromNode: string;
-  toNode: string;
-  isHidden: boolean | false;
-  parentType: NodeType;
-  parentName: string;
+    id: string;
+    fromConnector: string;
+    toConnector: string;
+    fromNode: string;
+    toNode: string;
+    isHidden: boolean | false;
+    parentType: NodeType;
+    parentName: string;
 }
 
 export interface Position {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 }
 
 export interface Attribute {
-  key: string;
-  value: string;
+    key: string;
+    value: string;
+    type: AttributeType,
+    inputType: AttributeInputType
 }
 
 export interface Node {
-  id: string;
-  name: string;
-  icon: IconType;
-  label: string;
-  type: NodeType;
-  position: Position;
-  connectors: Connector[];
-  isLocked?: boolean | false;
-  isSelected?: boolean | false;
-  Attributes?: Attribute[] | null;
-  isHidden: boolean | false;
+    id: string;
+    name: string;
+    icon: IconType;
+    label: string;
+    type: NodeType;
+    position: Position;
+    connectors: Connector[];
+    isLocked?: boolean | false;
+    isSelected?: boolean | false;
+    Attributes?: Attribute[] | null;
+    isHidden: boolean | false;
 }
 
 export interface LibNode {
-  id: string;
-  name: string;
-  label: string;
-  icon: string;
-  type: NodeType;
-  connectors: Connector[];
-  category: string;
-  Attributes?: Attribute[] | null;
-  // TODO: Add rules
+    id: string;
+    name: string;
+    label: string;
+    icon: string;
+    type: NodeType;
+    connectors: Connector[];
+    category: string;
+    Attributes?: Attribute[] | null;
 }
