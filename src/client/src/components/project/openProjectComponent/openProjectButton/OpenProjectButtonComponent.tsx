eddb9bc1da8bc2f2c96be8408de7973b8af81cab
mrlandro <helge.landro@bouvet.no>
2021-04-24 15:00:32 +0200

    Add guard to project button

--- src/client/src/components/project/openProjectComponent/openProjectButton/OpenProjectButtonComponent.tsx
+++ src/client/src/components/project/openProjectComponent/openProjectButton/OpenProjectButtonComponent.tsx
@@ -1,36 +1,45 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { get } from "../../../../redux/store/project/actions";
 import GetImg from "../../helpers/GetImg";
 import textResources from "../../../../textResources";
+import { RootState } from "../../../../redux/store";
+import { ProjectSimple } from "../../../../models/project";
 
 export const OpenProjectButtonComponent = ({ projectId }) => {
   console.log("test projectid: ", projectId);
   const dispatch = useDispatch();
   const [buttonHover, setbuttonHover] = useState(false);
+  const projects = useSelector<RootState>(
+    (state) => state.projectState.projectList
+  ) as ProjectSimple[];
+  console.log(projects);
+  const isVisible = projects ? projects.length > 0 : false;
 
   return (
-    <div className="open_project_button_wrapper">
-      <div
-        className="open_project_button"
-        onMouseOver={() => {
-          setbuttonHover(true);
-        }}
-        onMouseOut={() => {
-          setbuttonHover(false);
-        }}
-        onClick={() => dispatch(get(projectId))} //"95C10DAB-0DAD-4CBB-B33E-CA0A3CBC500C"
-      >
-        <p className="open_project_button_text">
-          {textResources.Project_recent_open}
-        </p>
-        {buttonHover ? (
-          <GetImg icon="WhiteRightArrowIcon" />
-        ) : (
-          <GetImg icon="" />
-        )}
+    isVisible && (
+      <div className="open_project_button_wrapper">
+        <div
+          className="open_project_button"
+          onMouseOver={() => {
+            setbuttonHover(true);
+          }}
+          onMouseOut={() => {
+            setbuttonHover(false);
+          }}
+          onClick={() => dispatch(get(projectId))} //"95C10DAB-0DAD-4CBB-B33E-CA0A3CBC500C"
+        >
+          <p className="open_project_button_text">
+            {textResources.Project_recent_open}
+          </p>
+          {buttonHover ? (
+            <GetImg icon="WhiteRightArrowIcon" />
+          ) : (
+            <GetImg icon="" />
+          )}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
