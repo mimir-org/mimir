0619dbc4ae8c2bacecf02b34dc290274dc6a39be
mrlandro <helge.landro@bouvet.no>
2021-04-27 11:29:13 +0200

    Update Edge model

---
+++ src/service/ModelBuilder.Core/Migrations/20210427081931_targetType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mb.Core.Migrations
+{
+    public partial class targetType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ParentName",
+                table: "Edge",
+                newName: "TargetType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TargetType",
+                table: "Edge",
+                newName: "ParentName");
+        }
+    }
+}
