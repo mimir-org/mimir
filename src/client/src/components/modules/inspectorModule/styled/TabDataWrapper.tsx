7e47981a717126bb388b3243563cbc1dd8ca3946
mrlandro <helge.landro@bouvet.no>
2021-04-28 15:41:22 +0200

    Add custom scrollbar

--- src/client/src/components/modules/inspectorModule/styled/TabDataWrapper.tsx
+++ src/client/src/components/modules/inspectorModule/styled/TabDataWrapper.tsx
@@ -6,7 +6,20 @@ const TabDataWrapper = styled.div`
   overflow-y: auto;
   height: 230px;
   width: inherit;
-  padding: 10px 0px 10px 10px;
+
+  ::-webkit-scrollbar {
+    width: 14px;
+    height: 18px;
+  }
+  ::-webkit-scrollbar-thumb {
+    height: 6px;
+    border: 4px solid rgba(0, 0, 0, 0);
+    background-clip: padding-box;
+    -webkit-border-radius: 7px;
+    background-color: rgba(0, 0, 0, 0.15);
+    -webkit-box-shadow: inset -1px -1px 0px rgba(0, 0, 0, 0.05),
+      inset 1px 1px 0px rgba(0, 0, 0, 0.05);
+  }
 `;
 
 export default TabDataWrapper;
