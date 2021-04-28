a55352f872e96092f9e51aa437e9c98b195d26ad
mrlandro <helge.landro@bouvet.no>
2021-04-28 21:36:09 +0200

    More styling

--- src/client/src/components/modules/inspectorModule/styled/TabColumn.tsx
+++ src/client/src/components/modules/inspectorModule/styled/TabColumn.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 const TabColumn = styled.div`
-  padding: 15px 0px 0px 15px;
+  padding: 15px 0px 0px 25px;
   margin: auto;
   margin-left: inherit;
-  min-width: 220px;
   width: 100%;
 
   position: ${(props) => (props.position ? "absolute" : "relative")};
