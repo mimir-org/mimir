4ea741f1f836f96543268ef82a0eb0fc419a7e0c
mrlandro <helge.landro@bouvet.no>
2021-04-30 08:54:06 +0200

    Refactor linking nodes to inspector

--- src/client/src/components/modules/explorerModule/hooks/useChangeNodeVisibility.tsx
+++ src/client/src/components/modules/explorerModule/hooks/useChangeNodeVisibility.tsx
@@ -2,13 +2,17 @@ import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { NodeType } from "../../../../models/project";
 import { changeNodeVisibility } from "../../../../redux/store/project/actions";
-import { Node } from "../../../../models/project";
-import store from "../../../../redux/store";
+import { useSelector } from "react-redux";
+import { Node, Project } from "../../../../models/project";
+import { RootState } from "../../../../redux/store";
 
 export const useChangeNodeVisibility = (node: Node, type: NodeType) => {
   const dispatch = useDispatch();
 
-  const project = store.getState().projectState.project;
+  const project = useSelector<RootState>(
+    (state) => state.projectState.project
+  ) as Project;
+
   const edges = project ? project.edges : [];
   const isParent = edges.find((x) => x.fromNode === node.id) ? true : false;
 
--- src/client/src/components/modules/inspectorModule/InspectorTabs.tsx
+++ src/client/src/components/modules/inspectorModule/InspectorTabs.tsx
@@ -1,10 +1,14 @@
 import { GetAttributes } from "./helpers";
 import { TabComponent } from ".";
-import { Attribute } from "../../../models/project";
-import store from "../../../redux/store";
+import { Attribute, Project } from "../../../models/project";
+import { RootState } from "../../../redux/store";
+import { useSelector } from "react-redux";
 
 const InspectorTabs = () => {
-  const project = store.getState().projectState.project;
+  const project = useSelector<RootState>(
+    (state) => state.projectState.project
+  ) as Project;
+
   const nodes = project ? project.nodes : [];
   const node = nodes.find((node) => node.isSelected);
   const nodeLabel = node ? node.label : "";
