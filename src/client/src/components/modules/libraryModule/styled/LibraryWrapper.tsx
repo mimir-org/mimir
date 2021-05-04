d62fb7c11f65ba77c0c7b9994c90d5de2c7e2112
mrlandro <helge.landro@bouvet.no>
2021-05-04 08:03:53 +0200

    Remove double border in Library

--- src/client/src/components/modules/libraryModule/styled/LibraryWrapper.tsx
+++ src/client/src/components/modules/libraryModule/styled/LibraryWrapper.tsx
@@ -4,8 +4,6 @@ const LibraryWrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 55%;
-  border: ${(props: { visible: boolean }) =>
-    props.visible ? "1px solid #cbcbcb" : "none"};
 `;
 
 export default LibraryWrapper;
