08d37292a43d13070cb066c6641d5e03483333a5
mrlandro <helge.landro@bouvet.no>
2021-05-07 13:17:53 +0200

    Finish rewrite Home component

--- src/client/src/componentLibrary/box/home/HomeBox.tsx
+++ src/client/src/componentLibrary/box/home/HomeBox.tsx
@@ -3,13 +3,6 @@ import styled from "styled-components";
 const HomeBox = styled.div`
   display: flex;
   height: 100%;
-
-  .middle_content {
-    flex: 1;
-    display: flex;
-    width: inherit;
-    flex-direction: column;
-  }
 `;
 
 export default HomeBox;
