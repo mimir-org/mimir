66d0045f3b2ad26b2c5e764e1b5605b9c51c919c
mrlandro <helge.landro@bouvet.no>
2021-05-07 09:55:27 +0200

    Two-way fullscreen mode

--- src/client/src/componentLibrary/controls/FullscreenBox.tsx
+++ src/client/src/componentLibrary/controls/FullscreenBox.tsx
@@ -1,16 +1,20 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { FullScreenIcon } from "../../assets/icons/controls";
 import { MODULE_TYPE } from "../../models/project";
+import { RootState } from "../../redux/store";
 import { CloseAllModules } from "../../redux/store/localStorage/localStorage";
 import { changeModuleVisibility } from "../../redux/store/modules/actions";
 import FullscreenButton from "./FullscreenButton";
 
 const FullscreenBox = () => {
   const dispatch = useDispatch();
+  const isOpen = useSelector<RootState>((state) =>
+    state.modules.type.find((x) => x.visible === true)
+  ) as boolean;
   const handleOnClick = () => {
-    dispatch(changeModuleVisibility(MODULE_TYPE.EXPLORER, false, true));
-    dispatch(changeModuleVisibility(MODULE_TYPE.INSPECTOR, false, true));
-    dispatch(changeModuleVisibility(MODULE_TYPE.LIBRARY, false, true));
+    dispatch(changeModuleVisibility(MODULE_TYPE.EXPLORER, !isOpen, true));
+    dispatch(changeModuleVisibility(MODULE_TYPE.INSPECTOR, !isOpen, true));
+    dispatch(changeModuleVisibility(MODULE_TYPE.LIBRARY, !isOpen, true));
     CloseAllModules();
   };
 
