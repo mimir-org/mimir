31b2e58fd2bf14c7110db6e4a7a03bff4a48aee2
Reidar Liabø <RLIAB@equinor.com>
2021-05-10 09:06:29 +0200

    Some small bugfixes

--- src/service/ModelBuilder.Core/Exceptions/ModelBuilderConfigurationException.cs
+++ src/service/ModelBuilder.Core/Exceptions/ModelBuilderConfigurationException.cs
@@ -5,6 +5,9 @@ namespace Mb.Core.Exceptions
     [Serializable]
     public class ModelBuilderConfigurationException : Exception
     {
+        public ModelBuilderConfigurationException(string message) : base(message)
+        {
 
+        }
     }
 }
