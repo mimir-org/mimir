569fa183e2434ab624bd87e3a6e4d2b3ccaa7cab
mrlandro <helge.landro@bouvet.no>
2021-05-14 16:02:57 +0200

    Fix splitview button

--- src/client/src/components/modules/explorerModule/SwitchViewComponent.tsx
+++ src/client/src/components/modules/explorerModule/SwitchViewComponent.tsx
@@ -1,21 +1,27 @@
-import { SplitViewIcon, ViewOffIcon } from "../../../assets/icons";
 import { FooterBox, FooterContent } from "../../../componentLibrary/box/footer";
 import { TextResources } from "../../../assets/textResources";
+import { LoadState } from "../../../redux/store/localStorage/localStorage";
+import { useState } from "react";
+import "./checkboxfooter.scss";
 
 export const SwitchViewComponent = () => {
+  const isVisible = LoadState("blockview");
+  const [isActive, SetIsActive] = useState(false);
+
+  const handleClick = () => {
+    SetIsActive(!isActive);
+  };
   return (
-    <>
-      <FooterBox>
-        <FooterContent>
-          <img src={ViewOffIcon} alt="view-icon" />
-          <p>{TextResources.Combined_view}</p>
-        </FooterContent>
-        <FooterContent>
-          <img src={SplitViewIcon} alt="view-icon" />
-          <p>{TextResources.Split_view}</p>
-        </FooterContent>
-      </FooterBox>
-    </>
+    <FooterBox visible={isVisible}>
+      <FooterContent onClick={handleClick} active={isActive}>
+        <label className={"checkbox-footer"}>
+          <input type="checkbox" checked={isActive} onChange={() => null} />
+          <span className="checkmark-footer"></span>
+          <label className="checkbox-footer_label"></label>
+        </label>
+        <p>{TextResources.Split_view}</p>
+      </FooterContent>
+    </FooterBox>
   );
 };
 
