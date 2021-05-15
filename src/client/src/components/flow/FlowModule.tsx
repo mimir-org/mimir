7a9e44f3f8b15fcc68a25371aa3fc93a7230956c
mrlandro <helge.landro@bouvet.no>
2021-05-15 23:36:52 +0200

    Fix routing

--- src/client/src/components/flow/FlowModule.tsx
+++ src/client/src/components/flow/FlowModule.tsx
@@ -1,9 +1,11 @@
 import { FlowTree, FlowBlock } from ".";
 import { VIEW_TYPE } from "../../models/project";
+import { SaveState } from "../../redux/store/localStorage";
 
 const FlowModule = ({ route }) => {
   if (route.type === undefined) {
     route.type = VIEW_TYPE.TREEVIEW;
+    SaveState(true, VIEW_TYPE.TREEVIEW);
   }
   return (
     <div className="dndflow">
