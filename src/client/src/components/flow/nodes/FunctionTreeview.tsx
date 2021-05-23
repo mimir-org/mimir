e11fc7af65c7b4b2b974a9f8538f26f2182f2e9b
Reidar Liabø <RLIAB@equinor.com>
2021-05-23 14:51:52 +0200

    Use of hover on connectors in treeview

--- src/client/src/components/flow/nodes/FunctionTreeview.tsx
+++ src/client/src/components/flow/nodes/FunctionTreeview.tsx
@@ -1,21 +1,32 @@
-import { memo, FC } from "react";
+import { memo, FC, useState } from "react";
 import { NodeProps, Handle } from "react-flow-renderer";
 import { RELATION_TYPE } from "../../../models/project";
 import { GetHandleType } from "../helpers";
 import { HandlerWrapper } from "../styled";
 
 const FunctionTreeview: FC<NodeProps> = ({ data }) => {
-  const handleHover = (e) => {
-    console.log("HOVER");
+  const [isHover, setIsHover] = useState(false);
+
+  const connectorIsVisible = (connector) => {
+    if (connector.relationType === RELATION_TYPE.PartOf && isHover)
+      return "true";
+    return "false";
   };
 
   return (
-    <div onMouseEnter={(e) => handleHover(e)}>
+    <div onMouseEnter={() => setIsHover(true)}>
       {data.connectors &&
         data.connectors.map((connector) => {
           const [typeHandler, positionHandler] = GetHandleType(connector);
+
           return (
-            <HandlerWrapper key={connector.id} display={"true"}>
+            <HandlerWrapper
+              onMouseEnter={() => setIsHover(true)}
+              onMouseLeave={(e) => setIsHover(false)}
+              key={connector.id}
+              display={connectorIsVisible(connector)}
+              position={positionHandler}
+            >
               <Handle
                 type={typeHandler}
                 position={positionHandler}
--- src/client/src/components/flow/styled/HandlerWrapper.tsx
+++ src/client/src/components/flow/styled/HandlerWrapper.tsx
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 import { FontSize } from "../../../componentLibrary";
+import { PlusIcon } from "../../../assets/icons/blockView";
 
 const HandlerWrapper = styled.div`
   .function-treeview-handler {
     visibility: ${(props: { display: string }) =>
       props.display === "false" ? `hidden` : `visible`};
-    /* width: 10px;
-    height: 10px;
-    background: #fff; */
+    width: 14px;
+    height: 15px;
+    background: none;
+    background-image: url(${PlusIcon});
+    border-radius: 0;
+    bottom: -10px;
+    top: ${(props: { position: string }) =>
+      props.position === "top" ? `-10px` : `auto`};
   }
 `;
 
