e229a19904fbfdbe48f79d77f0af5932c7ad553f
mrlandro <helge.landro@bouvet.no>
2021-05-26 00:46:06 +0200

    Add guards

--- src/client/src/components/flow/helpers/IsAspectSameType.ts
+++ src/client/src/components/flow/helpers/IsAspectSameType.ts
@@ -2,12 +2,12 @@ import { NODE_TYPE, Node, LibNode } from "../../../models/project";
 
 const IsAspectSameType = (aspectNode: Node, node: Node | LibNode) => {
   return (
-    (aspectNode.type === NODE_TYPE.ASPECT_LOCATION &&
-      node.type === NODE_TYPE.LOCATION) ||
-    (aspectNode.type === NODE_TYPE.ASPECT_FUNCTION &&
-      node.type === NODE_TYPE.FUNCTION) ||
-    (aspectNode.type === NODE_TYPE.ASPECT_PRODUCT &&
-      node.type === NODE_TYPE.PRODUCT)
+    (aspectNode?.type === NODE_TYPE.ASPECT_LOCATION &&
+      node?.type === NODE_TYPE.LOCATION) ||
+    (aspectNode?.type === NODE_TYPE.ASPECT_FUNCTION &&
+      node?.type === NODE_TYPE.FUNCTION) ||
+    (aspectNode?.type === NODE_TYPE.ASPECT_PRODUCT &&
+      node?.type === NODE_TYPE.PRODUCT)
   );
 };
 
