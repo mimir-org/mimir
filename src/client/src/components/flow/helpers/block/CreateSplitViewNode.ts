7ac4b2a84f99531b887f05dc14748c0e8f4baad5
mrlandro <helge.landro@bouvet.no>
2021-05-30 23:47:30 +0200

    Fix node size in split view

--- src/client/src/components/flow/helpers/block/CreateSplitViewNode.ts
+++ src/client/src/components/flow/helpers/block/CreateSplitViewNode.ts
@@ -2,6 +2,7 @@ import { Node } from "../../../../models/project";
 import { FlowElement } from "react-flow-renderer";
 import { SetSplitViewNodePosition } from ".";
 import { IsLocationNode } from "..";
+import { Size } from "../../../../componentLibrary";
 
 const CreateSplitViewNode = (node: Node): FlowElement => {
   let splitViewBlock = null;
@@ -11,6 +12,9 @@ const CreateSplitViewNode = (node: Node): FlowElement => {
 
   // Force node to fit Block
   const position = SetSplitViewNodePosition(node);
+
+  if (!node.width) node.width = Size.Node_Width;
+  if (!node.height) node.height = Size.Node_Height;
 
   splitViewBlock = {
     id: node.id,
