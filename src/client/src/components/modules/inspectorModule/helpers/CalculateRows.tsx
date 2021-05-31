f887d75f3c6820804053f70151f5aecd69f6a114
mrlandro <helge.landro@bouvet.no>
2021-05-31 09:13:39 +0200

    Update sorting of connectors

--- src/client/src/components/modules/inspectorModule/helpers/CalculateRows.tsx
+++ src/client/src/components/modules/inspectorModule/helpers/CalculateRows.tsx
@@ -5,7 +5,7 @@ const CalculateRows = (length: number): number => {
   let result = Math.ceil(rows);
 
   if (length === 20 && result === 4) result = 3;
-  if (length === 7) result = 1;
+  if (length === 7 || length === 8) result = 1;
 
   return result;
 };
