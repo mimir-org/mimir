b74a4950283b3a806cfa36159357776058be8ad2
mrlandro <helge.landro@bouvet.no>
2021-06-01 09:32:58 +0200

    Rewrite SetIndentLevel

--- src/client/src/assets/helpers/SetIndentLevel.tsx
+++ src/client/src/assets/helpers/SetIndentLevel.tsx
@@ -1,14 +1,7 @@
-import { CONNECTOR_TYPE, Node, Edge } from "../../models/project";
+import { Node, Edge } from "../../models/project";
 import store from "../../redux/store";
-import { IsPartOfTerminal } from "../../components/flow/helpers";
 
 const SetIndentLevel = (node: Node, count: number): number => {
-  const connector = node.connectors?.find(
-    (x) => IsPartOfTerminal(x) && x.type === CONNECTOR_TYPE.INPUT
-  );
-
-  if (connector === null) return count;
-
   const edge = store
     .getState()
     .projectState.project.edges.find((x) => x.toNode === node.id) as Edge;
@@ -20,7 +13,9 @@ const SetIndentLevel = (node: Node, count: number): number => {
     .getState()
     .projectState.project.nodes.find((x) => x.id === edge.fromNode);
 
-  if (!nextNode) return count;
+  if (!nextNode) {
+    return count;
+  }
 
   return SetIndentLevel(nextNode, count);
 };
