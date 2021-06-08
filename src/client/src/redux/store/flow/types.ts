2cac25f121cba90470f493ba446c032066404e43
mrlandro <helge.landro@bouvet.no>
2021-06-08 08:32:44 +0200

    Add guard to Visual Filter

--- src/client/src/redux/store/flow/types.ts
+++ src/client/src/redux/store/flow/types.ts
@@ -1,6 +1,5 @@
 import { ViewType } from "../../../models/project";
 export const CHANGE_FLOW_VIEW = "CHANGE_FLOW_VIEW";
-export const ADD_SELECTED_CONNECTOR = "ADD_SELECTED_CONNECTOR";
 
 export interface ChangeFlowView {
   type: typeof CHANGE_FLOW_VIEW;
