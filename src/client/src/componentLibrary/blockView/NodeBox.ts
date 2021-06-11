69b984ac94f4585a76cb7a1a32d015676d77e866
Helge Mikael <helge.landro@bouvet.no>
2021-06-11 14:16:18 +0200

    Fix styling on LocationNodes

--- src/client/src/componentLibrary/blockView/NodeBox.ts
+++ src/client/src/componentLibrary/blockView/NodeBox.ts
@@ -4,9 +4,12 @@ const NodeBox = styled.div`
   width: ${(props) => props.width}px;
   height: ${(props) => props.length}px;
   position: relative;
+
   .node-name {
     display: inline-block;
-    padding-top: 6px;
+    height: auto;
+    width: 95%;
+    overflow: hidden;
   }
 `;
 
