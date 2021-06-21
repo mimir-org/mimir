05521b1af0639fbd9ef61917deb68ff4223c6831
Reidar Liabø <RLIAB@equinor.com>
2021-06-21 15:51:43 +0200

    Bugfix Aspect on root nodes

--- src/client/src/components/flow/helpers/common/GetNodeType.ts
+++ src/client/src/components/flow/helpers/common/GetNodeType.ts
@@ -3,7 +3,6 @@ import { Aspect, Node } from "../../../../models";
 const GetNodeType = (node: Node) => {
     let typeName = node.isRoot ? "Aspect" : "";
     typeName += Aspect[node.aspect];
-    console.log(typeName);
     return typeName;
 };
 
