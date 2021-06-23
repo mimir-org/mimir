14a082482f01af5de7a528846be922ad514231ad
Reidar Liabø <RLIAB@equinor.com>
2021-06-24 01:12:31 +0200

    Fikset litt i type-editor

--- src/client/src/redux/sagas/typeEditor/saga.ts
+++ src/client/src/redux/sagas/typeEditor/saga.ts
@@ -1,131 +1,172 @@
 import { call, put } from "redux-saga/effects";
 import {
-  FETCHING_INITIAL_SUCCESS_OR_ERROR,
-  FETCHING_RDS_SUCCESS_OR_ERROR,
-  FETCHING_TERMINALS_SUCCESS_OR_ERROR,
-  FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
-  TypeEditorActionTypes,
+    FETCHING_INITIAL_SUCCESS_OR_ERROR,
+    FETCHING_RDS_SUCCESS_OR_ERROR,
+    FETCHING_TERMINALS_SUCCESS_OR_ERROR,
+    FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
+    CREATING_TYPE_SUCCESS_OR_ERROR,
+    TypeEditorActionTypes
 } from "../../store/typeEditor/types";
 
-import { get } from "../../../models/webclient";
+import {
+    get,
+    post,
+    GetBadResponseData,
+    ApiError
+} from "../../../models/webclient";
+
+// eslint-disable-next-line require-yield
+export function* createType(action) {
+    try {
+        const url = process.env.REACT_APP_API_BASE_URL + "typeeditor";
+        const response = yield call(post, url, action.payload.libraryType);
+
+        // This is a bad request
+        if (response.status === 400) {
+            const data = GetBadResponseData(response);
+            console.log(data, response);
+
+            const apiError = {
+                key: CREATING_TYPE_SUCCESS_OR_ERROR,
+                errorMessage: data.title,
+                errorData: data,
+            } as ApiError;
+
+            const payload = {
+                apiError: apiError,
+            };
+
+            yield put({
+                type: CREATING_TYPE_SUCCESS_OR_ERROR,
+                payload: payload,
+            });
+            return;
+        }
+
+
+
+    } catch (error) {
+
+    }
+}
 
 export function* getInitialData(action: TypeEditorActionTypes) {
-  try {
-    const aspectUrl = process.env.REACT_APP_API_BASE_URL + "typeeditor/aspects";
-    const statusUrl =
-      process.env.REACT_APP_API_BASE_URL + "typeeditor/statuses";
-    const objectsUrl =
-      process.env.REACT_APP_API_BASE_URL + "typeeditor/objects";
+    try {
+        const aspectUrl = process.env.REACT_APP_API_BASE_URL + "typeeditor/aspects";
+        const statusUrl =
+            process.env.REACT_APP_API_BASE_URL + "typeeditor/statuses";
+        const objectsUrl =
+            process.env.REACT_APP_API_BASE_URL + "typeeditor/objects";
 
-    const aspectResponse = yield call(get, aspectUrl);
-    const statusResponse = yield call(get, statusUrl);
-    const objectResponse = yield call(get, objectsUrl);
+        const aspectResponse = yield call(get, aspectUrl);
+        const statusResponse = yield call(get, statusUrl);
+        const objectResponse = yield call(get, objectsUrl);
 
-    const payload = {
-      aspects: aspectResponse.data,
-      objectTypes: objectResponse.data,
-      statuses: statusResponse.data,
-    };
+        const payload = {
+            aspects: aspectResponse.data,
+            objectTypes: objectResponse.data,
+            statuses: statusResponse.data,
+        };
 
-    yield put({
-      type: FETCHING_INITIAL_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  } catch (error) {
-    const payload = {
-      aspects: [],
-      objectTypes: [],
-      statuses: [],
-    };
+        yield put({
+            type: FETCHING_INITIAL_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    } catch (error) {
+        const payload = {
+            aspects: [],
+            objectTypes: [],
+            statuses: [],
+        };
 
-    yield put({
-      type: FETCHING_INITIAL_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  }
+        yield put({
+            type: FETCHING_INITIAL_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    }
 }
 
 export function* getRDS(action) {
-  try {
-    const rdsURL =
-      process.env.REACT_APP_API_BASE_URL +
-      "typeeditor/rds/" +
-      action.payload.aspect;
+    try {
+        const rdsURL =
+            process.env.REACT_APP_API_BASE_URL +
+            "typeeditor/rds/" +
+            action.payload.aspect;
 
-    const rdsResponse = yield call(get, rdsURL);
+        const rdsResponse = yield call(get, rdsURL);
 
-    const payload = {
-      Rds: rdsResponse.data,
-    };
+        const payload = {
+            Rds: rdsResponse.data,
+        };
 
-    yield put({
-      type: FETCHING_RDS_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  } catch (error) {
-    const payload = {
-      Rds: [],
-    };
+        yield put({
+            type: FETCHING_RDS_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    } catch (error) {
+        const payload = {
+            Rds: [],
+        };
 
-    yield put({
-      type: FETCHING_RDS_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  }
+        yield put({
+            type: FETCHING_RDS_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    }
 }
 
 export function* getTerminals(action) {
-  try {
-    const terminalURL =
-      process.env.REACT_APP_API_BASE_URL + "typeeditor/terminalsbycategory";
+    try {
+        const terminalURL =
+            process.env.REACT_APP_API_BASE_URL + "typeeditor/terminalsbycategory";
 
-    const terminalResponse = yield call(get, terminalURL);
+        const terminalResponse = yield call(get, terminalURL);
 
-    const payload = {
-      terminals: terminalResponse.data,
-    };
+        const payload = {
+            terminals: terminalResponse.data,
+        };
 
-    yield put({
-      type: FETCHING_TERMINALS_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  } catch (error) {
-    const payload = {
-      terminals: [],
-    };
+        yield put({
+            type: FETCHING_TERMINALS_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    } catch (error) {
+        const payload = {
+            terminals: [],
+        };
 
-    yield put({
-      type: FETCHING_TERMINALS_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  }
+        yield put({
+            type: FETCHING_TERMINALS_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    }
 }
 
 export function* getAttributes(action) {
-  try {
-    const attributesURL =
-      process.env.REACT_APP_API_BASE_URL +
-      "typeeditor/attributes/" +
-      action.payload.aspect;
+    try {
+        const attributesURL =
+            process.env.REACT_APP_API_BASE_URL +
+            "typeeditor/attributes/" +
+            action.payload.aspect;
 
-    const attributesResponse = yield call(get, attributesURL);
+        const attributesResponse = yield call(get, attributesURL);
 
-    const payload = {
-      AttributeType: attributesResponse.data,
-    };
+        const payload = {
+            AttributeType: attributesResponse.data,
+        };
 
-    yield put({
-      type: FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  } catch (error) {
-    const payload = {
-      AttributeType: [],
-    };
+        yield put({
+            type: FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    } catch (error) {
+        const payload = {
+            AttributeType: [],
+        };
 
-    yield put({
-      type: FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
-      payload: payload,
-    });
-  }
+        yield put({
+            type: FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
+            payload: payload,
+        });
+    }
 }
--- src/client/src/redux/store/typeEditor/reducers.ts
+++ src/client/src/redux/store/typeEditor/reducers.ts
@@ -1,172 +1,214 @@
 import { CreateLibraryType, Status, Aspect, ObjectType } from "../../../models";
+
 import {
-  FETCHING_RDS,
-  FETCHING_RDS_SUCCESS_OR_ERROR,
-  FETCHING_INITIAL_DATA,
-  FETCHING_INITIAL_SUCCESS_OR_ERROR,
-  FETCHING_TERMINALS,
-  FETCHING_TERMINALS_SUCCESS_OR_ERROR,
-  FETCHING_ATTRIBUTES,
-  FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
-  CHANGE_ASPECT,
-  CHANGE_OBJECTTYPE,
-  CHANGE_TYPENAME,
-  CHANGE_MODE,
-  CHANGE_STATUS,
-  CHANGE_RDS,
-  CHANGE_SEMANTICREFERENCE,
-  UPDATE_TERMINALTYPES,
-  UPDATE_ATTRIBUTETYPES,
-  TypeEditorActionTypes,
-  TypeEditorState,
+    FETCHING_RDS,
+    FETCHING_RDS_SUCCESS_OR_ERROR,
+    FETCHING_INITIAL_DATA,
+    FETCHING_INITIAL_SUCCESS_OR_ERROR,
+    FETCHING_TERMINALS,
+    FETCHING_TERMINALS_SUCCESS_OR_ERROR,
+    FETCHING_ATTRIBUTES,
+    FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR,
+    CHANGE_ASPECT,
+    CHANGE_OBJECTTYPE,
+    CHANGE_TYPENAME,
+    CHANGE_MODE,
+    CHANGE_STATUS,
+    CHANGE_RDS,
+    CHANGE_SEMANTICREFERENCE,
+    UPDATE_TERMINALTYPES,
+    UPDATE_ATTRIBUTETYPES,
+    CREATING_TYPE,
+    CREATING_TYPE_SUCCESS_OR_ERROR,
+    DELETE_TYPE_EDITOR_ERROR,
+    TypeEditorActionTypes,
+    TypeEditorState,
 } from "./types";
 
 const initialState: TypeEditorState = {
-  fetching: false,
-  creating: false,
-  mode: "NotSet",
-  createLibraryType: {
-    name: "",
-    status: Status.Draft,
-    aspect: Aspect.NotSet,
-    objectType: ObjectType.NotSet,
-    semanticReference: null,
-    rdsId: "",
-    terminalTypes: [],
-    attributeTypes: [] as string[],
-    terminalTypeId: null,
-  } as CreateLibraryType,
-  objectTypes: {},
-  aspects: {},
-  statuses: {},
-  rdsList: [],
-  terminals: [],
-  attributes: [],
+    fetching: false,
+    creating: false,
+    mode: "NotSet",
+    createLibraryType: {
+        name: "",
+        status: Status.Draft,
+        aspect: Aspect.NotSet,
+        objectType: ObjectType.NotSet,
+        semanticReference: null,
+        rdsId: "",
+        terminalTypes: [],
+        attributeTypes: [] as string[],
+        terminalTypeId: null,
+    } as CreateLibraryType,
+    objectTypes: {},
+    aspects: {},
+    statuses: {},
+    rdsList: [],
+    terminals: [],
+    attributes: [],
+    apiError: [],
 };
 
 export function typeEditorReducer(
-  state = initialState,
-  action: TypeEditorActionTypes
+    state = initialState,
+    action: TypeEditorActionTypes
 ): TypeEditorState {
-  switch (action.type) {
-    case FETCHING_INITIAL_DATA:
-      return {
-        ...state,
-        fetching: true,
-      };
-    case FETCHING_INITIAL_SUCCESS_OR_ERROR:
-      return {
-        ...state,
-        fetching: false,
-        aspects: action.payload.aspects,
-        statuses: action.payload.statuses,
-        objectTypes: action.payload.objectTypes,
-      };
-    case FETCHING_RDS:
-      return {
-        ...state,
-        fetching: true,
-      };
-    case FETCHING_RDS_SUCCESS_OR_ERROR:
-      return {
-        ...state,
-        fetching: false,
-        rdsList: action.payload.Rds ? action.payload.Rds : [],
-      };
-    case FETCHING_TERMINALS:
-      return {
-        ...state,
-        fetching: true,
-      };
-    case FETCHING_TERMINALS_SUCCESS_OR_ERROR:
-      return {
-        ...state,
-        fetching: false,
-        terminals: action.payload.terminals,
-      };
-    case FETCHING_ATTRIBUTES:
-      return {
-        ...state,
-        fetching: true,
-      };
-    case FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR:
-      return {
-        ...state,
-        fetching: false,
-        attributes: action.payload.AttributeType
-          ? action.payload.AttributeType
-          : [],
-      };
-    case CHANGE_MODE:
-      return {
-        ...state,
-        mode: action.payload.mode,
-      };
-    case CHANGE_ASPECT:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          aspect: action.payload.aspect,
-        },
-      };
-    case CHANGE_OBJECTTYPE:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          objectType: action.payload.objectType,
-        },
-      };
-    case CHANGE_TYPENAME:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          name: action.payload.typeName,
-        },
-      };
-    case CHANGE_STATUS:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          status: action.payload.status,
-        },
-      };
-    case CHANGE_RDS:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          rdsId: action.payload.rds,
-        },
-      };
-    case CHANGE_SEMANTICREFERENCE:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          semanticReference: action.payload.semanticReference,
-        },
-      };
-    case UPDATE_TERMINALTYPES:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          terminalTypes: action.payload.terminalTypes,
-        },
-      };
-    case UPDATE_ATTRIBUTETYPES:
-      return {
-        ...state,
-        createLibraryType: {
-          ...state.createLibraryType,
-          attributeTypes: action.payload.attributeTypes,
-        },
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case DELETE_TYPE_EDITOR_ERROR:
+            return {
+                ...state,
+                apiError: state.apiError
+                    ? state.apiError.filter((elem) => elem.key !== action.payload.key)
+                    : state.apiError,
+            };
+        case CREATING_TYPE:
+            return {
+                ...state,
+                fetching: true,
+                apiError: state.apiError
+                    ? state.apiError.filter((elem) => elem.key !== CREATING_TYPE)
+                    : state.apiError,
+            };
+        case CREATING_TYPE_SUCCESS_OR_ERROR:
+            console.log(action.payload.apiError);
+            return {
+                ...state,
+                fetching: false,
+                apiError: action.payload.apiError
+                    ? [...state.apiError, action.payload.apiError]
+                    : state.apiError,
+                createLibraryType: action.payload.apiError
+                    ? state.createLibraryType
+                    : {
+                        name: "",
+                        status: Status.Draft,
+                        aspect: Aspect.NotSet,
+                        objectType: ObjectType.NotSet,
+                        semanticReference: null,
+                        rdsId: "",
+                        terminalTypes: [],
+                        attributeTypes: [] as string[],
+                        terminalTypeId: null,
+                    } as CreateLibraryType
+            };
+        case FETCHING_INITIAL_DATA:
+            return {
+                ...state,
+                fetching: true,
+            };
+        case FETCHING_INITIAL_SUCCESS_OR_ERROR:
+            return {
+                ...state,
+                fetching: false,
+                aspects: action.payload.aspects,
+                statuses: action.payload.statuses,
+                objectTypes: action.payload.objectTypes,
+            };
+        case FETCHING_RDS:
+            return {
+                ...state,
+                fetching: true,
+            };
+        case FETCHING_RDS_SUCCESS_OR_ERROR:
+            return {
+                ...state,
+                fetching: false,
+                rdsList: action.payload.Rds ? action.payload.Rds : [],
+            };
+        case FETCHING_TERMINALS:
+            return {
+                ...state,
+                fetching: true,
+            };
+        case FETCHING_TERMINALS_SUCCESS_OR_ERROR:
+            return {
+                ...state,
+                fetching: false,
+                terminals: action.payload.terminals,
+            };
+        case FETCHING_ATTRIBUTES:
+            return {
+                ...state,
+                fetching: true,
+            };
+        case FETCHING_ATTRIBUTES_SUCCESS_OR_ERROR:
+            return {
+                ...state,
+                fetching: false,
+                attributes: action.payload.AttributeType
+                    ? action.payload.AttributeType
+                    : [],
+            };
+        case CHANGE_MODE:
+            return {
+                ...state,
+                mode: action.payload.mode,
+            };
+        case CHANGE_ASPECT:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    aspect: action.payload.aspect,
+                },
+            };
+        case CHANGE_OBJECTTYPE:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    objectType: action.payload.objectType,
+                },
+            };
+        case CHANGE_TYPENAME:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    name: action.payload.typeName,
+                },
+            };
+        case CHANGE_STATUS:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    status: action.payload.status,
+                },
+            };
+        case CHANGE_RDS:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    rdsId: action.payload.rds,
+                },
+            };
+        case CHANGE_SEMANTICREFERENCE:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    semanticReference: action.payload.semanticReference,
+                },
+            };
+        case UPDATE_TERMINALTYPES:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    terminalTypes: action.payload.terminalTypes,
+                },
+            };
+        case UPDATE_ATTRIBUTETYPES:
+            return {
+                ...state,
+                createLibraryType: {
+                    ...state.createLibraryType,
+                    attributeTypes: action.payload.attributeTypes,
+                },
+            };
+        default:
+            return state;
+    }
 }
