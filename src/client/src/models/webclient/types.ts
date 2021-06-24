b7e8a6fcd7c0a684188f2bca88bc574e94281b6b
Helge Mikael <helge.landro@bouvet.no>
2021-06-24 17:12:25 +0200

    Refactor

--- src/client/src/models/webclient/types.ts
+++ src/client/src/models/webclient/types.ts
@@ -1,107 +1,110 @@
 import { authProvider } from "../../providers/authProvider";
 
 export interface ApiError {
-    key: string,
-    errorMessage: string,
-    errorData: BadRequestData
+  key: string;
+  errorMessage: string;
+  errorData: BadRequestData;
 }
 export interface BadRequestData {
-    title: string,
-    items: BadRequestDataItem[]
+  title: string;
+  items: BadRequestDataItem[];
 }
 export interface BadRequestDataItem {
-    key: string,
-    value: string
+  key: string;
+  value: string;
 }
 
 export interface HttpResponse<T> extends Response {
-    data?: T;
+  data?: T;
 }
 
 export async function getUserProfile() {
-    try {
-        var idToken = await authProvider.getIdToken();
-        if (idToken) {
-            var user = { displayName: idToken.idToken.name };
-            return user;
-        }
-    } catch (error) {
-        throw new Error("Could not load user data");
+  try {
+    var idToken = await authProvider.getIdToken();
+    if (idToken) {
+      var user = { displayName: idToken.idToken.name };
+      return user;
     }
+  } catch (error) {
+    throw new Error("Could not load user data");
+  }
 }
 
 const RequestInitDefault: RequestInit = {
-    method: "get",
-    cache: "no-store",
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Methods": "DELETE, POST, GET, PUT, OPTIONS",
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Access-Control-Allow-Headers":
-            "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-        Origin: "http://localhost:3000",
-        "Cache-Control": "no-cache",
-    },
+  method: "get",
+  cache: "no-store",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Methods": "DELETE, POST, GET, PUT, OPTIONS",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Headers":
+      "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+    Origin: "http://localhost:3000",
+    "Cache-Control": "no-cache",
+  },
 };
 
 export async function http<T>(request: RequestInfo): Promise<HttpResponse<T>> {
-    let response: HttpResponse<T> = null;
+  let response: HttpResponse<T> = null;
 
-    try {
-        response = await fetch(request);
-        if (!response || !response.ok) {
-            if (((response.status >= 200 && response.status < 300) || response.status === 400)) {
-                response.data = await response.json();
-                return response;
-            } else {
-                if (response.status === 404) {
-                    throw new Error("Ther server is unavailable");
-                }
-                response.data = await response.json();
-                throw new Error(response.data.toString());
-            }
-        } else {
-            if (response.status === 204) {
-                throw new Error(response.statusText);
-            }
-
-            response.data = await response.json();
-            return response;
+  try {
+    response = await fetch(request);
+    if (!response || !response.ok) {
+      if (
+        (response.status >= 200 && response.status < 300) ||
+        response.status === 400
+      ) {
+        response.data = await response.json();
+        return response;
+      } else {
+        if (response.status === 404) {
+          throw new Error("Ther server is unavailable");
         }
-    } catch (e) {
-        throw new Error("Ther server is unavailable");
+        response.data = await response.json();
+        throw new Error(response.data.toString());
+      }
+    } else {
+      if (response.status === 204) {
+        throw new Error(response.statusText);
+      }
+
+      response.data = await response.json();
+      return response;
     }
+  } catch (e) {
+    throw new Error("Ther server is unavailable");
+  }
 }
 
 export async function get<T>(
-    path: string,
-    args: RequestInit = { method: "get" }
+  path: string,
+  args: RequestInit = { method: "get" }
 ): Promise<HttpResponse<T>> {
-    const req = { ...RequestInitDefault, ...args };
-    var idTokenResponse = await authProvider.getAccessToken();
-    req.headers["Authorization"] = "Bearer " + idTokenResponse.accessToken;
-    return await http<T>(new Request(path, req));
+  const req = { ...RequestInitDefault, ...args };
+  var idTokenResponse = await authProvider.getAccessToken();
+  req.headers["Authorization"] = "Bearer " + idTokenResponse.accessToken;
+  return await http<T>(new Request(path, req));
 }
 
 export async function post<T>(
-    path: string,
-    body: any,
-    args: RequestInit = { method: "post", body: JSON.stringify(body) }
+  path: string,
+  body: any,
+  args: RequestInit = { method: "post", body: JSON.stringify(body) }
 ): Promise<HttpResponse<T>> {
-    const req = { ...RequestInitDefault, ...args };
-    var idTokenResponse = await authProvider.getAccessToken();
-    req.headers["Authorization"] = "Bearer " + idTokenResponse.accessToken;
-    return await http<T>(new Request(path, req));
+  const req = { ...RequestInitDefault, ...args };
+  var idTokenResponse = await authProvider.getAccessToken();
+  req.headers["Authorization"] = "Bearer " + idTokenResponse.accessToken;
+  return await http<T>(new Request(path, req));
 }
 
 export async function put<T>(
-    path: string,
-    body: any,
-    args: RequestInit = { method: "put", body: JSON.stringify(body) }
+  path: string,
+  body: any,
+  args: RequestInit = { method: "put", body: JSON.stringify(body) }
 ): Promise<HttpResponse<T>> {
-    const req = { ...RequestInitDefault, ...args };
-    var idTokenResponse = await authProvider.getAccessToken();
-    req.headers["Authorization"] = "Bearer " + idTokenResponse.accessToken;
-    return await http<T>(new Request(path, req));
+  const req = { ...RequestInitDefault, ...args };
+  var idTokenResponse = await authProvider.getAccessToken();
+  req.headers["Authorization"] = "Bearer " + idTokenResponse.accessToken;
+  return await http<T>(new Request(path, req));
 }
--- src/client/src/redux/store/common/actions.ts
+++ src/client/src/redux/store/common/actions.ts
@@ -1,17 +1,21 @@
-import { CommonActionTypes, FETCHING_CONTRACTORS, DELETE_COMMON_ERROR } from "./types";
+import {
+  CommonActionTypes,
+  FETCHING_CONTRACTORS,
+  DELETE_COMMON_ERROR,
+} from "./types";
 
 export function getContractors(): CommonActionTypes {
-    return {
-        type: FETCHING_CONTRACTORS,
-        payload: null,
-    };
+  return {
+    type: FETCHING_CONTRACTORS,
+    payload: null,
+  };
 }
 
 export function deleteCommonError(key: string) {
-    return {
-        type: DELETE_COMMON_ERROR,
-        payload: {
-            key
-        }
-    }
+  return {
+    type: DELETE_COMMON_ERROR,
+    payload: {
+      key,
+    },
+  };
 }
