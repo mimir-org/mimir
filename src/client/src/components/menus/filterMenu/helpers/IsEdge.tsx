03e572cec43815fd705e002360c6ffa5582670f5
Helge Mikael <helge.landro@bouvet.no>
2021-07-01 19:15:33 +0200

    Remove terminals from Visual Filter

---
+++ src/client/src/components/menus/filterMenu/helpers/IsEdge.tsx
@@ -0,0 +1,13 @@
+import { Edge } from "../../../../models";
+
+const IsEdge = (element) => {
+  const edgeType = Object.values(Edge);
+
+  return (
+    element.type === undefined ||
+    element.type === null ||
+    edgeType.some((x) => x === element.type?.toString())
+  );
+};
+
+export default IsEdge;
