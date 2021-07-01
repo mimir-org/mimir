987ae07c271a30a8a139391fba9ac7d2080beeb5
Reidar Liabø <RLIAB@equinor.com>
2021-07-01 18:11:56 +0200

    Testing

--- src/service/Modules/MicrosoftSqlServerModule/MicrosoftSqlServerModule.cs
+++ src/service/Modules/MicrosoftSqlServerModule/MicrosoftSqlServerModule.cs
@@ -34,19 +34,19 @@ namespace MicrosoftSqlServerModule
             var password = Environment.GetEnvironmentVariable("DatabaseConfiguration_Password");
 
             if (!string.IsNullOrEmpty(dataSource))
-                dbConfig.DataSource = dataSource;
+                dbConfig.DataSource = dataSource.Trim();
 
-            if (!string.IsNullOrEmpty(port) && int.TryParse(port, out var portAsInt))
+            if (!string.IsNullOrEmpty(port) && int.TryParse(port.Trim(), out var portAsInt))
                 dbConfig.Port = portAsInt;
 
             if (!string.IsNullOrEmpty(initialCatalog))
-                dbConfig.InitialCatalog = initialCatalog;
+                dbConfig.InitialCatalog = initialCatalog.Trim();
 
             if (!string.IsNullOrEmpty(dbUser))
-                dbConfig.DbUser = dbUser;
+                dbConfig.DbUser = dbUser.Trim();
 
             if (!string.IsNullOrEmpty(password))
-                dbConfig.Password = password;
+                dbConfig.Password = password.Trim();
 
             services.AddSingleton(Options.Create(dbConfig));
 
