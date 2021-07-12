4c78974156bebe6bd40f58bb3c08e9d6f07fd2c5
Pål-Jørgen Valdersnes <Pal-Jorgen.Valdersnes@bouvet.no>
2021-07-12 10:34:25 +0200

    Migration to fix Node in accordance to new model

--- src/service/ModelBuilder.Core/Migrations/20210712083336_Node_Change_Types_Add_Cost.Designer.cs
+++ src/service/ModelBuilder.Core/Migrations/20210712083336_Node_Change_Types_Add_Cost.Designer.cs
@@ -4,14 +4,16 @@ using Mb.Models.Configurations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Mb.Core.Migrations
 {
     [DbContext(typeof(ModelBuilderDbContext))]
-    partial class ModelBuilderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210712083336_Node_Change_Types_Add_Cost")]
+    partial class Node_Change_Types_Add_Cost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
