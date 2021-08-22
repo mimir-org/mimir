70950eb68f0346443356f75f0711caa6a1a48bc6
Reidar Liabø <RLIAB@equinor.com>
2021-08-22 21:33:05 +0200

    Bugfic save type

---
+++ src/service/ModelBuilder.Core/Repositories/Contracts/INodeTypeTerminalTypeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mb.Models.Configurations;
+using Mb.Models.Data;
+
+namespace Mb.Core.Repositories.Contracts
+{
+    public interface INodeTypeTerminalTypeRepository : IGenericRepository<ModelBuilderDbContext, NodeTypeTerminalType>
+    {
+    }
+}
