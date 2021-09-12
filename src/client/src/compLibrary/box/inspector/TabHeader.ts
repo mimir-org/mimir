eea397fdd6c9783ce52b2734fc997c02a55dc7c9
Helge Mikael <helge.landro@bouvet.no>
2021-09-12 14:12:33 +0200

    Redesign inspector tabs

--- src/client/src/compLibrary/box/inspector/TabHeader.ts
+++ src/client/src/compLibrary/box/inspector/TabHeader.ts
@@ -6,17 +6,17 @@ const TabHeader = styled.div`
   position: relative;
   top: -44px;
   box-sizing: border-box;
-  border-top-right-radius: 8px;
+  border-top-right-radius: 6px;
   border-top-left-radius: 6px;
   color: ${Color.Black};
-  margin-right: 8px;
-  box-shadow: -4px 0 4px -5px #888, 4px 0 4px -5px #888;
-
-  height: ${(props: { active: string }) => (props.active ? "39px" : "35px")};
+  margin-right: 7px;
+  height: 33px;
   margin-top: ${(props: { active: string }) => (props.active ? "0px" : "9px")};
 
+  box-shadow: ${(props) => !props.active && "#888 0px 2px 4px"};
+
   background-color: ${(props) =>
-    props.active ? Color.GreyInspector : props.color};
+    props.active ? props.background : props.color};
 
   padding: ${(props: { active: string }) =>
     props.active ? "12px 20px 0px 20px;" : "8px 20px 0px 20px"};
