7699103648d71605f0eb3ecb953cc7f180f7fd9b
Reidar Liabø <RLIAB@equinor.com>
2021-09-15 09:12:32 +0200

    Removed purpose migration

--- src/service/ModelBuilder.Models/Data/Node.cs
+++ src/service/ModelBuilder.Models/Data/Node.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Mb.Models.Data.Enums;
 using Mb.Models.Enums;
 using Newtonsoft.Json;
