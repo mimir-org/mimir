b90abe51baccad398032dacdfb3184357aa0a746
Helge Mikael <helge.landro@bouvet.no>
2021-09-15 11:32:05 +0200

    Add handlers to Node object

--- src/client/src/components/flow/block/TerminalsComponent.tsx
+++ src/client/src/components/flow/block/TerminalsComponent.tsx
@@ -1,47 +1,22 @@
-import red from "../../../redux/store";
-import { Connector, Aspect } from "../../../models";
+import { Connector } from "../../../models";
 import { TerminalsBox, TerminalsElement } from "../../../compLibrary/blockView";
-import {
-  GetConnectorIcon,
-  GetConnectorName,
-  IsLocationTerminal,
-} from "../helpers/common";
+import { GetConnectorIcon, GetConnectorName } from "../helpers/common";
 
 interface Props {
   isOpen: boolean;
   list: Connector[];
-  aspect: Aspect;
   width: number;
   onClick: (conn: Connector) => void;
 }
 
-const TerminalsComponent = ({
-  isOpen,
-  list,
-  aspect,
-  width,
-  onClick,
-}: Props) => {
-  const splitView = red.store.getState().splitView.visible as boolean;
-  if (!splitView) {
-    if (aspect === Aspect.Function)
-      list = list.filter((x) => !IsLocationTerminal(x));
-  }
-
-  return (
-    <TerminalsBox visible={isOpen} width={width}>
-      {list.map((conn) => (
-        <TerminalsElement key={conn.id} onClick={() => onClick(conn)}>
-          <p className="text"> {GetConnectorName(conn)}</p>
-          <img
-            src={GetConnectorIcon(conn.color)}
-            alt="icon"
-            className="button"
-          />
-        </TerminalsElement>
-      ))}
-    </TerminalsBox>
-  );
-};
-
+const TerminalsComponent = ({ isOpen, list, width, onClick }: Props) => (
+  <TerminalsBox visible={isOpen} width={width}>
+    {list.map((conn) => (
+      <TerminalsElement key={conn.id} onClick={() => onClick(conn)}>
+        <p className="text"> {GetConnectorName(conn)}</p>
+        <img src={GetConnectorIcon(conn.color)} alt="icon" className="button" />
+      </TerminalsElement>
+    ))}
+  </TerminalsBox>
+);
 export default TerminalsComponent;
