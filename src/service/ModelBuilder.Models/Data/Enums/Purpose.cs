b19bb62a2e8d2fe52b7d4a0b3061333124a61a43
Reidar Liabø <RLIAB@equinor.com>
2021-09-15 11:37:48 +0200

    Purpose migration

--- src/service/ModelBuilder.Models/Data/Enums/Purpose.cs
+++ src/service/ModelBuilder.Models/Data/Enums/Purpose.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections.Generic;
-using Mb.Models.Enums;
-using Newtonsoft.Json;
+﻿using Mb.Models.Enums;
 
 namespace Mb.Models.Data.Enums
 {
     public class Purpose : EnumBase
     {
         public Discipline Discipline { get; set; }
-
-        [JsonIgnore]
-        public virtual ICollection<BlobData> Purposes { get; set; }
-
         public override string Key => $"{Name}-{InternalType}-{Discipline}";
     }
 }
