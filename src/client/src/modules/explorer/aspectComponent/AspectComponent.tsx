792da4a917e94e67929bb79de54fbad0bd16f2df
henrik-knudsen <henrik.knudsen@bouvet.no>
2021-09-16 13:09:40 +0200

    Refactor. Remove AspectElement, render nodes flat from ProjectComponent.

--- src/client/src/modules/explorer/aspectComponent/AspectComponent.tsx
+++ src/client/src/modules/explorer/aspectComponent/AspectComponent.tsx
@@ -1,45 +1,40 @@
-import { useState } from "react";
 import { ExpandIcon, CollapseIcon } from "../../../assets/icons/common";
 import { Node, Project } from "../../../models";
 import { IsAspectNode } from "../../../components/flow/helpers/common";
-import { AspectElement } from ".";
 import { AspectBox } from "../../../compLibrary/box/aspect";
 import { Checkbox, CheckboxBlock } from "../checkboxComponent";
 import { IsBlockView } from "../../../components/flow/helpers/block";
-import { OnExpandClick } from "../handlers";
-import {
-  GetAspectIcon,
-  GetAspectColor,
-  SetIndentLevel,
-} from "../../../assets/helpers";
-import { HasChildren, IsAncestorInSet } from "../helpers/ParentNode";
+import { GetAspectIcon } from "../../../assets/helpers";
 
 interface Props {
   node: Node;
   label: string;
+  indent: number;
   project: Project;
+  isRoot: boolean;
+  isLeaf: boolean;
+  expanded: boolean;
+  onElementExpanded: (expanded: boolean, nodeId: string) => void;
 }
-export const AspectComponent = ({ node, label, project }: Props) => {
-  const [expanded, setExpanded] = useState(true);
+export const AspectComponent = ({
+  node,
+  label,
+  project,
+  expanded,
+  indent,
+  isLeaf,
+  isRoot,
+  onElementExpanded,
+}: Props) => {
   const expandIcon = expanded ? ExpandIcon : CollapseIcon;
   const aspectIcon = GetAspectIcon(node);
-  const color = GetAspectColor(node, true);
-  const nodes = project.nodes;
-  const children = nodes.filter((x) => !IsAspectNode(x)) ?? [];
-  const [closedChildren, setClosedChildren] = useState(new Set<string>());
-
-  const onExpandElement = (_expanded: boolean, nodeId: string) => {
-    _expanded ? closedChildren.delete(nodeId) : closedChildren.add(nodeId);
-    setClosedChildren((_) => new Set(closedChildren));
-  };
-
-  const areAncestorsExpanded = (elem: Node): boolean =>
-    !IsAncestorInSet(elem, closedChildren);
 
   return (
     <>
-      <AspectBox color={color}>
-        <img src={aspectIcon} alt="aspect-icon" className="icon"></img>
+      <AspectBox indent={indent} node={node} isRoot={isRoot}>
+        {IsAspectNode(node) && (
+          <img src={aspectIcon} alt="aspect-icon" className="icon"></img>
+        )}
         <div className="checkbox_container">
           {IsBlockView() ? (
             <CheckboxBlock node={node} inputLabel={label} />
@@ -47,32 +42,15 @@ export const AspectComponent = ({ node, label, project }: Props) => {
             <Checkbox node={node} project={project} inputLabel={label} />
           )}
         </div>
-        <img
-          className="expandIcon"
-          src={expandIcon}
-          alt="expand-icon"
-          onClick={() => OnExpandClick(setExpanded, expanded)}
-        ></img>
+        {!isLeaf && (
+          <img
+            className="expandIcon"
+            src={expandIcon}
+            alt="expand-icon"
+            onClick={() => onElementExpanded(!expanded, node.id)}
+          ></img>
+        )}
       </AspectBox>
-      {expanded &&
-        children.map((elem) => {
-          if (elem.aspect === node.aspect && areAncestorsExpanded(elem)) {
-            const indent = elem.level ?? SetIndentLevel(elem, 0);
-            return (
-              <AspectElement
-                key={elem.id}
-                node={elem}
-                label={elem.label ?? elem.name}
-                indent={indent}
-                project={project}
-                isLeaf={!HasChildren(elem)}
-                expanded={!closedChildren.has(elem.id)}
-                onElementExpanded={onExpandElement}
-              />
-            );
-          }
-          return null;
-        })}
     </>
   );
 };
