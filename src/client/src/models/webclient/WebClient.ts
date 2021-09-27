64492d8a7677bc9a4d04e0c584a8408231e31420
Reidar Liabø <RLIAB@equinor.com>
2021-09-27 14:42:38 +0200

    Added commit menu element

--- src/client/src/models/webclient/WebClient.ts
+++ src/client/src/models/webclient/WebClient.ts
@@ -4,80 +4,80 @@ import { RequestInitDefault, HttpResponse } from ".";
 import { TextResources } from "../../assets/text";
 
 export const Token = async () => {
-  const account = msalInstance.getActiveAccount();
+    const account = msalInstance.getActiveAccount();
 
-  if (!account) {
-    throw Error(TextResources.Error_NoActiveAccount);
-  }
+    if (!account) {
+        throw Error(TextResources.Error_NoActiveAccount);
+    }
 
-  const response = await msalInstance.acquireTokenSilent({
-    ...loginRequest,
-    account: account,
-  });
+    const response = await msalInstance.acquireTokenSilent({
+        ...loginRequest,
+        account: account,
+    });
 
-  return `Bearer ${response.accessToken}`;
+    return `Bearer ${response.accessToken}`;
 };
 
 export async function http<T>(request: RequestInfo): Promise<HttpResponse<T>> {
-  let response: HttpResponse<T> = null;
+    let response: HttpResponse<T> = null;
 
-  try {
-    response = await fetch(request);
-    if (!response || !response.ok) {
-      if (
-        (response.status >= 200 && response.status < 300) ||
-        response.status === 400
-      ) {
-        response.data = await response.json();
-        return response;
-      } else {
-        if (response.status === 404) {
-          throw new Error(TextResources.Error_ServerUnavailable);
+    try {
+        response = await fetch(request);
+        if (!response || !response.ok) {
+            if (
+                (response.status >= 200 && response.status < 300) ||
+                response.status === 400
+            ) {
+                response.data = await response.json();
+                return response;
+            } else {
+                if (response.status === 404) {
+                    throw new Error(TextResources.Error_ServerUnavailable);
+                }
+                response.data = await response.json();
+                throw new Error(response.data.toString());
+            }
+        } else {
+            if (response.status === 204) {
+                return response;
+            }
+
+            response.data = await response.json();
+            return response;
         }
-        response.data = await response.json();
-        throw new Error(response.data.toString());
-      }
-    } else {
-      if (response.status === 204) {
-        throw new Error(response.statusText);
-      }
-
-      response.data = await response.json();
-      return response;
+    } catch (e) {
+        throw new Error(TextResources.Error_ServerUnavailable);
     }
-  } catch (e) {
-    throw new Error(TextResources.Error_ServerUnavailable);
-  }
 }
 
 export async function get<T>(
-  path: string,
-  args: RequestInit = { method: "get" }
+    path: string,
+    args: RequestInit = { method: "get" }
 ): Promise<HttpResponse<T>> {
-  const req = { ...RequestInitDefault, ...args };
-  const token = await Token();
-  req.headers["Authorization"] = token;
-  return await http<T>(new Request(path, req));
+    const req = { ...RequestInitDefault, ...args };
+    const token = await Token();
+    req.headers["Authorization"] = token;
+    return await http<T>(new Request(path, req));
 }
 
 export async function post<T>(
-  path: string,
-  body: any,
-  args: RequestInit = { method: "post", body: JSON.stringify(body) }
+    path: string,
+    body: any,
+    args: RequestInit = { method: "post", body: JSON.stringify(body) }
 ): Promise<HttpResponse<T>> {
-  const token = await Token();
-  const req = { ...RequestInitDefault, ...args };
-  req.headers["Authorization"] = token;
-  return await http<T>(new Request(path, req));
+    const token = await Token();
+    const req = { ...RequestInitDefault, ...args };
+    req.headers["Authorization"] = token;
+    return await http<T>(new Request(path, req));
 }
 
 export async function put<T>(
-  path: string,
-  body: any,
-  args: RequestInit = { method: "put", body: JSON.stringify(body) }
+    path: string,
+    body: any,
+    args: RequestInit = { method: "put", body: JSON.stringify(body) }
 ): Promise<HttpResponse<T>> {
-  const token = await Token();
-  const req = { ...RequestInitDefault, ...args };
-  req.headers["Authorization"] = token;
-  return await http<T>(new Request(path, req));
+    const token = await Token();
+    const req = { ...RequestInitDefault, ...args };
+    req.headers["Authorization"] = token;
+    return await http<T>(new Request(path, req));
 }
