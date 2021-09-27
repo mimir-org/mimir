e25c66c498f9bc1617ff6b782256913bb19cdf2c
Reidar Liabø <RLIAB@equinor.com>
2021-09-27 20:34:07 +0200

    CombinedAttributeFilter support, frontend and backend

--- src/client/src/redux/store/common/actions.ts
+++ src/client/src/redux/store/common/actions.ts
@@ -1,29 +1,37 @@
 import {
-  CommonActionTypes,
-  FETCHING_CONTRACTORS,
-  FETCHING_STATUSES,
-  DELETE_COMMON_ERROR,
+    CommonActionTypes,
+    FETCHING_CONTRACTORS,
+    FETCHING_STATUSES,
+    DELETE_COMMON_ERROR,
+    FETCHING_COMBINED_ATTRIBUTE_FILTERS,
 } from "./types";
 
 export function getContractors(): CommonActionTypes {
-  return {
-    type: FETCHING_CONTRACTORS,
-    payload: null,
-  };
+    return {
+        type: FETCHING_CONTRACTORS,
+        payload: null,
+    };
 }
 
 export function deleteCommonError(key: string) {
-  return {
-    type: DELETE_COMMON_ERROR,
-    payload: {
-      key,
-    },
-  };
+    return {
+        type: DELETE_COMMON_ERROR,
+        payload: {
+            key,
+        },
+    };
 }
 
 export function getStatuses(): CommonActionTypes {
-  return {
-    type: FETCHING_STATUSES,
-    payload: null,
-  };
+    return {
+        type: FETCHING_STATUSES,
+        payload: null,
+    }
+}
+
+export function getAttributeFilters(): CommonActionTypes {
+    return {
+        type: FETCHING_COMBINED_ATTRIBUTE_FILTERS,
+        payload: null
+    };
 }
--- src/service/ModelBuilder.Core/Controllers/V1/CommonController.cs
+++ src/service/ModelBuilder.Core/Controllers/V1/CommonController.cs
@@ -106,5 +106,31 @@ namespace Mb.Core.Controllers.V1
                 return StatusCode(500, "Internal Server Error");
             }
         }
+
+        /// <summary>
+        /// Returns a list of all combined attributes
+        /// </summary>
+        /// <returns>List of combined attributes></returns>
+        [HttpGet("attribute-filter")]
+        [ProducesResponseType(typeof(ICollection<CombinedAttributeFilter>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetCombinedAttributes()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var result = _commonService.GetAllCombinedAttributeFilters().ToList();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Internal Server Error: Error: {e.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
     }
 }
