d25ef1e02c5f73c65edcdda91deb39afa0709538
Kai Andre Hovden <kai.hovden@bouvet.no>
2021-09-29 23:09:22 +0200

    Backend - Attributes deletion. WIP

--- src/service/ModelBuilder.Core/Repositories/EdgeRepository.cs
+++ src/service/ModelBuilder.Core/Repositories/EdgeRepository.cs
@@ -13,12 +13,14 @@ namespace Mb.Core.Repositories
         private readonly IAttributeRepository _attributeRepository;
         private readonly ITransportRepository _transportRepository;
         private readonly IInterfaceRepository _interfaceRepository;
+        private readonly IConnectorRepository _connectorRepository;
 
-        public EdgeRepository(ModelBuilderDbContext dbContext, IAttributeRepository attributeRepository, ITransportRepository transportRepository, IInterfaceRepository interfaceRepository) : base(dbContext)
+        public EdgeRepository(ModelBuilderDbContext dbContext, IAttributeRepository attributeRepository, ITransportRepository transportRepository, IInterfaceRepository interfaceRepository, IConnectorRepository connectorRepository) : base(dbContext)
         {
             _attributeRepository = attributeRepository;
             _transportRepository = transportRepository;
             _interfaceRepository = interfaceRepository;
+            _connectorRepository = connectorRepository;
         }
 
         public IEnumerable<Edge> UpdateInsert(ICollection<Edge> original, Project project)
@@ -68,13 +70,56 @@ namespace Mb.Core.Repositories
                 }
 
                 if (edge.Transport?.Attributes != null)
-                    _attributeRepository.Attach(edge.Transport.Attributes, EntityState.Deleted);
+                {
+                    foreach (var transportAttribute in edge.Transport.Attributes)
+                    {
+                        await _attributeRepository.Delete(transportAttribute.Id);
+                    }
+                }
+
+
+                if (edge.Interface?.Attributes != null)
+                {
+                    foreach (var interfaceAttribute in edge.Interface.Attributes)
+                    {
+                        await _attributeRepository.Delete(interfaceAttribute.Id);
+                    }
+                }
 
                 if (edge.Transport != null)
+                {
+                    
                     await _transportRepository.Delete(edge.Transport.Id);
+                    
+                    var transportAttributes = _attributeRepository.FindBy(x =>
+                        x.TerminalId == edge.Transport.InputTerminalId ||
+                        x.TerminalId == edge.Transport.OutputTerminalId);
+
+                    foreach (var transportAttribute in transportAttributes)
+                    {
+                        await _attributeRepository.Delete(transportAttribute.Id);
+                    }
+
+                    //await _connectorRepository.Delete(edge.Transport?.InputTerminalId);
+                    //await _connectorRepository.Delete(edge.Transport?.OutputTerminalId);
+                }
 
                 if (edge.Interface != null)
+                {
                     await _interfaceRepository.Delete(edge.Interface.Id);
+
+                    var interfaceAttributes = _attributeRepository.FindBy(x =>
+                        x.TerminalId == edge.Transport.InputTerminalId ||
+                        x.TerminalId == edge.Transport.OutputTerminalId);
+
+                    foreach (var interfaceAttribute in interfaceAttributes)
+                    {
+                        await _attributeRepository.Delete(interfaceAttribute.Id);
+                    }
+
+                    //await _connectorRepository.Delete(edge.Interface?.InputTerminalId);
+                    //await _connectorRepository.Delete(edge.Interface?.OutputTerminalId);
+                }
 
                 await Delete(edge.Id);
             }
