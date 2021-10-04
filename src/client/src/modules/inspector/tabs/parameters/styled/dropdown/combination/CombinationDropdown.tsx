670194fb06a001e75abcb0496fc2597c3c3371e5
Henrik Knudsen <henrik.knudsen@bouvet.no>
2021-10-04 14:03:07 +0200

    Add Select All option in Parameter Combination dropdown.
    Behavior: If not all list items are selected, select all selects all items currently not selected. If all items are selected, select all deselects all items. Select all is checked if all items are selected, unchecked if not.

--- src/client/src/modules/inspector/tabs/parameters/styled/dropdown/combination/CombinationDropdown.tsx
+++ src/client/src/modules/inspector/tabs/parameters/styled/dropdown/combination/CombinationDropdown.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { TextResources } from "../../../../../../../assets/text";
 import { MenuWrapper, MenuHeader, MenuList, MenuListItem, CheckboxWrapper } from "./styled";
 import { ExpandWhiteIcon, CollapseWhiteIcon } from "../../../../../../../assets/icons/common";
@@ -15,13 +15,58 @@ interface Props {
 const EntityDropdown = ({ items, selectedItems, keyProp, onChange, color }: Props) => {
   const [isListOpen, setIsListOpen] = useState(false);
 
-  const IsItemSelected = (item: CombinedAttribute): boolean => {
-    return !!selectedItems.find((other) => item.combined === other.combined);
+  const IsItemSelected = useCallback(
+    (item: CombinedAttribute): boolean =>
+      !!selectedItems.find((other) => item.combined === other.combined),
+    [selectedItems]
+  );
+
+  const areAllItemsSelected = useMemo(
+    () => selectedItems.filter(IsItemSelected).length === items?.length,
+    [items, selectedItems, IsItemSelected]
+  );
+
+  const renderSelectAll = () => {
+    return (
+      <div
+        onClick={() =>
+          items
+            .filter((item) => selectedItems.includes(item) === areAllItemsSelected)
+            .forEach((item) => onChange(item, areAllItemsSelected))
+        }
+      >
+        <MenuListItem color={color}>
+          <p>{TextResources.Inspector_Params_Combinations_Select_All}</p>
+          <CheckboxWrapper>
+            <label className={"checkbox-block"}>
+              <input type="checkbox" checked={areAllItemsSelected} readOnly={true} />
+              <span className="checkmark-block"></span>
+            </label>
+          </CheckboxWrapper>
+        </MenuListItem>
+      </div>
+    );
+  };
+
+  const renderListItem = (item: CombinedAttribute) => {
+    return (
+      <div onClick={() => onChange(item, IsItemSelected(item))} key={item[keyProp]}>
+        <MenuListItem color={color}>
+          <p>{item.combined}</p>
+          <CheckboxWrapper>
+            <label className={"checkbox-block"}>
+              <input type="checkbox" checked={IsItemSelected(item)} readOnly={true} />
+              <span className="checkmark-block"></span>
+            </label>
+          </CheckboxWrapper>
+        </MenuListItem>
+      </div>
+    );
   };
 
   return (
     <MenuWrapper tabIndex={0} onBlur={() => setIsListOpen(false)}>
-      <div onClick={(e) => setIsListOpen(!isListOpen)}>
+      <div onClick={() => setIsListOpen(!isListOpen)}>
         <MenuHeader open={isListOpen} color={color}>
           <p>{TextResources.Inspector_Params_Combinations}</p>
           <img src={isListOpen ? ExpandWhiteIcon : CollapseWhiteIcon} alt="expand-icon" />
@@ -29,21 +74,8 @@ const EntityDropdown = ({ items, selectedItems, keyProp, onChange, color }: Prop
       </div>
       {isListOpen && (
         <MenuList>
-          {items?.map((item) => {
-            return (
-              <div onClick={() => onChange(item, IsItemSelected(item))} key={item[keyProp]}>
-                <MenuListItem color={color}>
-                  <p>{item.combined}</p>
-                  <CheckboxWrapper>
-                    <label className={"checkbox-block"}>
-                      <input type="checkbox" checked={IsItemSelected(item)} readOnly={true} />
-                      <span className="checkmark-block"></span>
-                    </label>
-                  </CheckboxWrapper>
-                </MenuListItem>
-              </div>
-            );
-          })}
+          {renderSelectAll()}
+          {items?.map((item) => renderListItem(item))}
         </MenuList>
       )}
     </MenuWrapper>
