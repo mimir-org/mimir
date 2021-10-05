1c5eb51b7091d1a1495085202870a26addfe5a25
Helge Mikael <helge.landro@bouvet.no>
2021-10-05 23:23:59 +0200

    Fix fullscreen issues

--- src/client/src/compLibrary/controls/handlers/OnToggleClick.tsx
+++ src/client/src/compLibrary/controls/handlers/OnToggleClick.tsx
@@ -1,8 +1,10 @@
 import { Size } from "../../";
+import { SetPanelHeight } from "../../../modules/inspector/helpers";
 import { changeInspectorHeight } from "../../../modules/inspector/redux/height/actions";
 import { setModulesVisibility } from "../../../redux/store/modules/actions";
 
 const OnToggleClick = (dispatch: any, isOpen: boolean) => {
+  SetPanelHeight(isOpen ? Size.ModuleClosed : Size.ModuleOpen);
   dispatch(changeInspectorHeight(isOpen ? Size.ModuleClosed : Size.ModuleOpen));
   dispatch(setModulesVisibility(!isOpen, true));
 };
