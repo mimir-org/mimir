c78604fbd690ddd817148978c2ec9137d24a21fa
Henrik Knudsen <henrik.knudsen@bouvet.no>
2021-10-06 10:49:34 +0200

    Refactor.

--- src/client/src/modules/inspector/tabs/parameters/styled/ParametersRowWrapper.ts
+++ src/client/src/modules/inspector/tabs/parameters/styled/ParametersRowWrapper.ts
@@ -5,9 +5,7 @@ const ParametersRowWrapper = styled.div`
   display: flex;
   flex-direction: column;
 
-  border-color: ${Color.DarkGrey};
-  border-style: solid;
-  border-width: 1px 0px 0px 0px;
+  border-top: 1px solid ${Color.DarkGrey};
 
   :last-child {
     margin-bottom: 200px;
