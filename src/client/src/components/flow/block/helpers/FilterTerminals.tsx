eaaca52e90f5af8ff9f0e36919df0887fd49fe44
Helge Mikael <helge.landro@bouvet.no>
2021-10-07 11:59:38 +0200

    Rewrite FilterTerminals

--- src/client/src/components/flow/block/helpers/FilterTerminals.tsx
+++ src/client/src/components/flow/block/helpers/FilterTerminals.tsx
@@ -1,51 +1,32 @@
 import { SortTerminals } from ".";
 import { Connector, Node } from "../../../../models";
-import {
-  IsFunction,
-  IsProduct,
-  IsInputTerminal,
-  IsLocation,
-  IsLocationTerminal,
-  IsOutputTerminal,
-  IsTransportTerminal,
-} from "../../helpers";
+import { IsLocation, IsLocationTerminal, IsTransportTerminal } from "../../helpers";
 
 /**
  * Component to filter the terminals displayed on the nodes in BlockView.
- * @param node the selected node
+ * @param n the selected node
  * @param splitView is splitView activated
  * @param splitNode selected SplitNode, if any
  * @returns a call to SortTerminals that sorts the filtered list.
  */
-const FilterTerminals = (node: Node, splitView: boolean, splitNode: Node) => {
-  console.log({ splitNode });
+const FilterTerminals = (n: Node, splitView: boolean, splitNode: Node) => {
   let terminals: Connector[] = [];
-  if (node === undefined) return [];
+  if (n === undefined) return [];
 
-  if (!splitView) {
-    node.connectors?.forEach((c) => {
-      if (IsFunction(node) || IsProduct(node)) IsTransportTerminal(c) && terminals.push(c);
-      else if (IsLocation(node)) IsLocationTerminal(c) && terminals.push(c);
-    });
-  }
-
-  if (splitView) {
-    if (!splitNode) {
-      node.connectors?.forEach((c) => {
-        if (IsFunction(node) || IsProduct(node)) IsTransportTerminal(c) && terminals.push(c);
-      });
-    } else {
-      node.connectors?.forEach((c) => {
-        if ((IsFunction(node) || IsProduct(node)) && IsLocation(splitNode))
-          IsOutputTerminal(c) && IsLocationTerminal(c) && terminals.push(c);
-
-        if ((IsFunction(node) || IsProduct(node)) && !IsLocation(splitNode)) IsTransportTerminal(c) && terminals.push(c);
-        else if (IsLocation(node)) IsInputTerminal(c) && IsLocationTerminal(c) && terminals.push(c);
-      });
-    }
-  }
-
+  n.connectors?.forEach((c) => {
+    validateFilterTerminal(n, splitNode, c, splitView) && terminals.push(c);
+  });
   return SortTerminals(terminals);
 };
+
+function validateFilterTerminal(n: Node, splitNode: Node, c: Connector, splitView: boolean) {
+  if (!splitView) return (IsLocation(n) && IsLocationTerminal(c)) || IsTransportTerminal(c);
+  if (splitView && !splitNode) return IsTransportTerminal(c);
+
+  if (splitView && splitNode) {
+    if (IsLocation(splitNode)) return IsLocationTerminal(c);
+    return IsTransportTerminal(c);
+  }
+}
 
 export default FilterTerminals;
--- src/client/src/components/flow/block/helpers/index.ts
+++ src/client/src/components/flow/block/helpers/index.ts
@@ -2,7 +2,6 @@ export { default as GetBlockHandleType } from "./GetBlockHandleType";
 export { default as ShowBlockViewEdge } from "./ShowBlockViewEdge";
 export { default as IsBlockView } from "./IsBlockView";
 export { default as IsSplitView } from "./IsSplitView";
-export { default as FilterTerminals } from "./FilterTerminals";
 export { default as SortTerminals } from "./SortTerminals";
 export { default as FindSelectedBlockNode } from "./FindSelectedBlockNode";
 export { default as SetTerminalOrder } from "./SetTerminalOrder";
@@ -10,5 +9,6 @@ export { default as OnBlockClick } from "./OnBlockClick";
 export { default as FindNodeByDataId } from "./FindNodeByDataId";
 export { default as FindAllEdges } from "./FindAllEdges";
 export { default as SetSplitViewBackground } from "./SetSplitViewBackground";
+export { default as FilterTerminals } from "./FilterTerminals";
 
 export { default as GetBlockEdgeTypes } from "./GetBlockEdgeTypes";
