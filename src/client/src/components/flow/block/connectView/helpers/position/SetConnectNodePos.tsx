202414ae3a1c9d5c8181dc35a74ad218bfac30f2
Helge Mikael <helge.landro@bouvet.no>
2021-10-07 12:17:50 +0200

    Rewrite SetConnectNodePos

--- src/client/src/components/flow/block/connectView/helpers/position/SetConnectNodePos.tsx
+++ src/client/src/components/flow/block/connectView/helpers/position/SetConnectNodePos.tsx
@@ -1,24 +1,21 @@
 import { Node } from "../../../../../../models";
 import { Size } from "../../../../../../compLibrary";
-import { CalculateXPos, CalculateYPos } from ".";
 
 /**
  * Function to force child nodes in the ConnectViewBlock to fit in the parent block.
  * @param node
- * @param mainConnectNodeId
+ * @param mainConnectNodeId - the parent block
  * @param connectNodes - the selected child nodes in ConnectView
  * @param allNodes - all nodes in Mimir
  * @returns an X and Y position in form of numbers
  */
 const SetConnectNodePos = (node: Node, mainConnectNodeId: string, connectNodes: Node[], allNodes: Node[]) => {
   const mainNode = allNodes.find((x) => x.id === mainConnectNodeId);
-
   let xPos = mainNode ? mainNode.positionBlockX : node.positionBlockX;
   let yPos = mainNode ? mainNode.positionBlockY : node.positionBlockY;
 
-  xPos = CalculateXPos(node, xPos, connectNodes);
-  yPos = CalculateYPos(node, yPos, connectNodes);
-  if (connectNodes?.length === 0) yPos -= Size.Node_Length;
+  xPos = setXPos(node, xPos, connectNodes);
+  yPos = setYPos(node, yPos, connectNodes);
 
   if (node !== mainNode) {
     node.positionBlockX = xPos;
@@ -27,5 +24,41 @@ const SetConnectNodePos = (node: Node, mainConnectNodeId: string, connectNodes: 
 
   return { x: node.positionBlockX, y: node.positionBlockY };
 };
+
+function setXPos(node: Node, xPos: number, connectNodes: Node[]) {
+  const leftMargin = 35;
+  const rightMargin = 239;
+  const center = 135;
+
+  if (connectNodes.length === 1) return xPos + center;
+
+  connectNodes?.forEach((n, i) => {
+    if (i % 2 === 0 && node.id === n.id) xPos += leftMargin;
+    if (i % 2 !== 0 && node.id === n.id) xPos += rightMargin;
+  });
+
+  return xPos;
+}
+
+function setYPos(node: Node, yPos: number, connectNodes: Node[]) {
+  const yMargin = 30;
+  const marginTop = Size.Node_Length - 15;
+  let increaseYPos = 1;
+  yPos += marginTop;
+  let test = 4;
+  let increment = 1;
+
+  connectNodes?.forEach((elem, i) => {
+    if (i > 1 && node.id === elem.id) {
+      // TODO: make algo and remove bad variables
+      if (i > 3 && i < 6) increaseYPos += test * increment;
+      if (i >= 6 && i < 8) increaseYPos += test * (increment * 2);
+      if (i >= 8 && i < 10) increaseYPos += test * (increment * 3);
+      return (yPos += Size.Node_Length + yMargin * increaseYPos);
+    }
+  });
+
+  return yPos;
+}
 
 export default SetConnectNodePos;
--- src/client/src/components/flow/block/connectView/helpers/position/index.ts
+++ src/client/src/components/flow/block/connectView/helpers/position/index.ts
@@ -1,4 +1,2 @@
 export { default as CalculateMenuPos } from "./CalculateMenuPos";
 export { default as SetConnectNodePos } from "./SetConnectNodePos";
-export { default as CalculateXPos } from "./CalculateXPos";
-export { default as CalculateYPos } from "./CalculateYPos";
