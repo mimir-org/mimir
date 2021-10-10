a1a9dbb498e8a763d3e799e0c3b13d3b0aadde83
Helge Mikael <helge.landro@bouvet.no>
2021-10-10 12:49:18 +0200

    Fix multiple id's in SplitView

--- src/client/src/components/flow/creators/helpers/DrawSplitViewChildren.tsx
+++ src/client/src/components/flow/creators/helpers/DrawSplitViewChildren.tsx
@@ -1,6 +1,7 @@
 import { Elements } from "react-flow-renderer";
 import { CreateSplitViewChildNode } from "..";
 import { Node, Edge } from "../../../../models";
+import { IsDirectChild } from "../../block/helpers";
 
 /**
  * Component to draw all SplitView children nodes in BlockView.
@@ -13,7 +14,7 @@ const DrawSplitViewChildren = (edges: Edge[], nodes: Node[], splitViewNode: Node
   if (splitViewNode) {
     edges.forEach((edge) => {
       if (edge.fromNodeId === splitViewNode.id && splitViewNode.aspect === edge.toNode.aspect) {
-        const toNode = nodes.find((node) => node.id === edge.toNodeId);
+        const toNode = nodes.find((node) => node.id === edge.toNodeId && IsDirectChild(node, splitViewNode));
         if (toNode) elements.push(CreateSplitViewChildNode(toNode));
       }
     });
