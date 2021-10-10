5f9da96f789d8691f23cfc7d08376121d3669dd2
Reidar Liabø <RLIAB@equinor.com>
2021-10-10 15:02:53 +0200

    Created support for translate names in dropdowns

--- src/client/src/components/modules/typeEditorModule/helpers/GetPurposes.tsx
+++ src/client/src/components/modules/typeEditorModule/helpers/GetPurposes.tsx
@@ -1,11 +1,18 @@
-import { DropDownItem } from "../../../../compLibrary/dropdown/Dropdown";
+import { DropDownItem } from "../../../../compLibrary/dropdown/typeEditor/Dropdown";
 import { Purpose } from "../../../../models";
+import { CreateId } from "../../../flow/helpers";
 
 const GetPurposes = (purposes: Purpose[]): DropDownItem[] => {
   const categories = [] as DropDownItem[];
-  categories.push({ name: "Purposes", items: [] } as DropDownItem);
+  categories.push({ id: CreateId(), name: "Purposes", description: "Purpose", image: null, items: [] });
   purposes.forEach((purpose) => {
-    categories[0].items.push(purpose);
+    categories[0].items.push({
+      id: purpose.id,
+      name: purpose.name,
+      description: purpose.name,
+      image: null,
+      items: [],
+    });
   });
   return categories;
 };
