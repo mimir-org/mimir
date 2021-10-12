9839aa4fdfa19145e4d21b9b7b52c64f39e2d8db
Helge Mikael <helge.landro@bouvet.no>
2021-10-12 17:49:19 +0200

    Update rules for terminals display in BlockView

--- src/client/src/components/flow/block/helpers/FilterTerminals.tsx
+++ src/client/src/components/flow/block/helpers/FilterTerminals.tsx
@@ -1,6 +1,13 @@
 import { SortTerminals } from ".";
 import { Connector, Node } from "../../../../models";
-import { IsLocation, IsLocationTerminal, IsTransportTerminal } from "../../helpers";
+import {
+  IsLocation,
+  IsProduct,
+  IsFunction,
+  IsLocationTerminal,
+  IsTransportTerminal,
+  IsFulfilledByTerminal,
+} from "../../helpers";
 
 /**
  * Component to filter the terminals displayed on the nodes in BlockView.
@@ -20,10 +27,19 @@ const FilterTerminals = (n: Node, splitView: boolean, splitNode: Node) => {
 };
 
 function validate(n: Node, splitNode: Node, c: Connector, splitView: boolean) {
-  if (!splitView) return (IsLocation(n) && IsLocationTerminal(c)) || IsTransportTerminal(c);
-  if (splitView && splitNode && IsLocation(splitNode)) return IsLocationTerminal(c);
+  if (!splitView) {
+    return (
+      (IsLocation(n) && IsLocationTerminal(c)) ||
+      (IsProduct(n) && IsFulfilledByTerminal(c)) ||
+      (IsFunction(n) && IsTransportTerminal(c))
+    );
+  }
 
-  return IsTransportTerminal(c);
+  if (splitView && splitNode && IsLocation(splitNode)) return IsLocationTerminal(c);
+  if (splitView && splitNode && IsProduct(splitNode)) return IsFulfilledByTerminal(c);
+  if (splitView && splitNode && IsFunction(splitNode)) return IsFulfilledByTerminal(c);
+
+  return false;
 }
 
 export default FilterTerminals;
