14f8e4d075e349357bc5b5eba175f4da367db21f
Helge Mikael <helge.landro@bouvet.no>
2021-10-12 14:49:20 +0200

    Flexible ToolBar

--- src/client/src/components/header/ToolBar.tsx
+++ src/client/src/components/header/ToolBar.tsx
@@ -28,11 +28,11 @@ const ToolBar = ({ libOpen, explorerOpen, treeView, filterMenuOpen, electro }: P
   return (
     <ToolBarBox id="ToolBar" libOpen={libOpen} explorerOpen={explorerOpen}>
       <OptionsBox>
-        <OptionsElement onClick={() => Click.OnFilter(dispatch, filterMenuOpen)}>
+        <OptionsElement treeView={treeView} onClick={() => Click.OnFilter(dispatch, filterMenuOpen)}>
           <img src={Icons.Filter} alt="VisualFilter" />
         </OptionsElement>
         {!treeView && (
-          <OptionsElement onClick={() => Click.OnElectro(dispatch, electro)}>
+          <OptionsElement treeView={treeView} onClick={() => Click.OnElectro(dispatch, electro)}>
             <img src={!electro ? Icons.Vertical : Icons.Horizontal} alt="Electro" />
           </OptionsElement>
         )}
