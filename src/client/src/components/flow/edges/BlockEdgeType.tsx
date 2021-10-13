df12261dcdb330b50baec5b6a781425b1357fa6f
Helge Mikael <helge.landro@bouvet.no>
2021-10-13 11:16:58 +0200

    Update BlockEdgeType

--- src/client/src/components/flow/edges/BlockEdgeType.tsx
+++ src/client/src/components/flow/edges/BlockEdgeType.tsx
@@ -1,10 +1,20 @@
 import "./BlockEdge.scss";
-import { GetClassName, GetStyle } from "./helpers";
-import { Connector } from "../../../models";
+import { Connector, RelationType, Aspect, Node } from "../../../models";
 import { ArrowHeadType, getBezierPath, getMarkerEnd, getSmoothStepPath } from "react-flow-renderer";
 import { IsLocationTerminal } from "../helpers";
+import { GetStyle } from "./helpers";
 
-const BlockEdgeType = ({ id, sourceX, sourceY, targetX, targetY, data, markerEndId }) => {
+export default function BlockEdgeType({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  data,
+  markerEndId,
+}) {
   const markerEnd = getMarkerEnd(ArrowHeadType.ArrowClosed, markerEndId);
   const fromConn = data.source.connectors?.find((conn: Connector) => conn.id === data.edge.fromConnectorId) as Connector;
   const hasLocation = IsLocationTerminal(fromConn);
@@ -14,37 +24,75 @@ const BlockEdgeType = ({ id, sourceX, sourceY, targetX, targetY, data, markerEnd
     sourceY,
     targetX,
     targetY,
+    sourcePosition,
+    targetPosition,
   });
 
   const bezier = getBezierPath({
     sourceX,
     sourceY,
+    sourcePosition,
     targetX,
     targetY,
+    targetPosition,
   });
 
-  return !hasLocation ? (
-    <path id={id} style={GetStyle(fromConn)} className="react-flow__edge-path" d={smooth} markerEnd={markerEnd} />
-  ) : (
+  const getClassName = (source: Node): string => {
+    let defaultClassName = "react-flow__edge-path ";
+
+    const sourceConn = data.source.connectors?.find(
+      (x: { id: any }) => x.id === data.edge.fromConnector.id
+    ) as Connector;
+
+    switch (sourceConn?.relationType) {
+      case RelationType.HasLocation:
+        defaultClassName += "has-location";
+        break;
+      case RelationType.FulfilledBy:
+        defaultClassName += "fulfilled-by";
+        break;
+      default:
+        defaultClassName += "";
+    }
+
+    switch (source.aspect) {
+      case Aspect.Product:
+        defaultClassName += "-product";
+        break;
+      case Aspect.Function:
+        defaultClassName += "-function";
+        break;
+      default:
+        defaultClassName += "";
+    }
+
+    return defaultClassName;
+  };
+
+  return (
     <>
-      <path
-        id={id}
-        style={GetStyle(fromConn)}
-        className={GetClassName(data.source) + ""}
-        d={bezier}
-        markerEnd={markerEnd}
-      />
-      <path
-        id={id}
-        style={GetStyle(fromConn)}
-        className={GetClassName(data.source) + "--dashed"}
-        d={bezier}
-        markerEnd={markerEnd}
-      />
+      {!hasLocation ? (
+        <path id={id} style={GetStyle(fromConn)} className="react-flow__edge-path" d={smooth} markerEnd={markerEnd} />
+      ) : (
+        <>
+          <path
+            id={id}
+            style={GetStyle(fromConn)}
+            className={getClassName(data.source) + ""}
+            d={bezier}
+            markerEnd={markerEnd}
+          />
+          <path
+            id={id}
+            style={GetStyle(fromConn)}
+            className={getClassName(data.source) + "--dashed"}
+            d={bezier}
+            markerEnd={markerEnd}
+          />
+        </>
+      )}
     </>
   );
-};
-
-export default BlockEdgeType;
+}
 // const arrowStyle = document.body.style;
 // arrowStyle.setProperty("--arrow-color", fromConnector?.color);
--- src/client/src/components/flow/edges/helpers/index.ts
+++ src/client/src/components/flow/edges/helpers/index.ts
@@ -1,2 +1,1 @@
-export { default as GetClassName } from "./GetClassName";
 export { default as GetStyle } from "./GetStyle";
