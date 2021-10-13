103b28ac67d8119fe52b3aeaa3abc9f11d9087a5
Helge Mikael <helge.landro@bouvet.no>
2021-10-13 11:01:41 +0200

    Remove console log info

--- src/client/src/components/flow/block/helpers/OnBlockClick.tsx
+++ src/client/src/components/flow/block/helpers/OnBlockClick.tsx
@@ -15,7 +15,6 @@ const OnBlockClick = (e: any, dispatch: any, project: Project) => {
   // Handle select Edge
   if (e.target.classList.contains("react-flow__edge-path")) {
     const edge = project.edges.find((x) => x.id === e.target.id);
-    console.log({ edge });
     dispatch(setActiveEdge(edge?.id, true));
     dispatch(setActiveBlockNode(null));
     dispatch(changeInspectorTab(0));
