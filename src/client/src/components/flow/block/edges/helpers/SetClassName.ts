4850c34938d7e7a53aa3c73eaed7623894bc025b
Helge Mikael <helge.landro@bouvet.no>
2021-10-17 22:01:37 +0200

    Refactor edges

--- src/client/src/components/flow/block/edges/helpers/SetClassName.ts
+++ src/client/src/components/flow/block/edges/helpers/SetClassName.ts
@@ -3,13 +3,12 @@ import { IsFulfilledByTerminal, IsFunction, IsLocationTerminal, IsProduct } from
 
 const SetClassName = (data: any): string => {
   let defaultClassName = "react-flow__edge-path ";
-  const sourceConn = data.source.connectors?.find((x: { id: any }) => x.id === data.edge.fromConnector.id) as Connector;
+  const sourceConn = data.source.connectors?.find((x: Connector) => x.id === data.edge.fromConnector.id) as Connector;
 
   if (IsLocationTerminal(sourceConn)) defaultClassName += "has-location";
   else if (IsFulfilledByTerminal(sourceConn)) defaultClassName += "fulfilled-by";
-  else if (IsProduct(data.source)) defaultClassName += "-product";
+  if (IsProduct(data.source)) defaultClassName += "-product";
   else if (IsFunction(data.source)) defaultClassName += "-function";
-  else defaultClassName += "";
 
   return defaultClassName;
 };
