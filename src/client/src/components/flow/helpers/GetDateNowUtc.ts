5f57c76af649c12ba7d67c5c68186d3d844743f5
Reidar Liabø <RLIAB@equinor.com>
2021-10-20 22:53:43 +0200

    Fix comment

--- src/client/src/components/flow/helpers/GetDateNowUtc.ts
+++ src/client/src/components/flow/helpers/GetDateNowUtc.ts
@@ -1,9 +1,7 @@
 /**
- * Function to find a node's childNode
+ * Function to find datetime now in UTC timestamp
  * @returns a new date as UTC
  */
-
-// TODO: this is used to traverse down one step in BlockView. How to find the correct child node will be defined later.
 const GetDateNowUtc = (): Date => {
   const currTimestamp = Date.now();
   const utcDateString = (new Date(currTimestamp)).toUTCString();
