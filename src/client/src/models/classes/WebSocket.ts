bfe77f015d150ade09ff91109c2f48892a5ea0a1
Reidar Liabø <RLIAB@equinor.com>
2021-10-20 22:28:45 +0200

    Initial setup websocket removed unused code

--- src/client/src/models/classes/WebSocket.ts
+++ src/client/src/models/classes/WebSocket.ts
@@ -1,11 +1,7 @@
 import { HubConnectionBuilder, HubConnection } from "@microsoft/signalr";
 import { Dispatch } from "react";
-// import { updatePositionReceived } from "../../redux/store/project/actions";
-// import { NodePosition } from "../Types";
-// import { default as reduxStore } from "./../../redux/store/index";
 
 let instance = null;
-
 class WebSocket {
 
   private _connection: HubConnection;
@@ -18,7 +14,7 @@ class WebSocket {
       return instance;
     }
 
-    this._running = false; // REACT_APP_SOCKET_METHODS
+    this._running = false;
 
     this._connection = new HubConnectionBuilder()
       .withUrl(`${process.env.REACT_APP_SOCKET_BASE_URL}mimir`)
@@ -34,26 +30,16 @@ class WebSocket {
     if (this._connection && !this._running) {
       this._connection
         .start()
-        .then((result) => {
+        .then(() => {
           console.log("Websocket connected to server");
           this._running = true;
 
-          const methods = JSON.parse(process.env.REACT_APP_SOCKET_METHODS) as string[];
-
-          methods.forEach(name => {
-            this._connection.on(name, (data) => {
-              if (name === "ReceiveNode") {
-                console.log(data);
-                // const position = data as NodePosition;
-                // reduxStore.store.dispatch(updatePositionReceived(position.nodeId, position.x, position.y));
-              }
-            });
+          this._connection.on("ReceiveNode", (data) => {
+            console.log(data);
           });
-
-
         })
         .catch((e: any) => {
-          // console.log("Connection failed: ", e);
+
         })
     }
   }
