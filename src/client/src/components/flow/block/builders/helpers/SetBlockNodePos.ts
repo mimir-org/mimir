e4414042f9c9c45ea3dc82328c0a8e59062d93d0
Helge Mikael <helge.landro@bouvet.no>
2021-10-24 20:40:13 +0200

    Refactor setPosition

--- src/client/src/components/flow/block/builders/helpers/SetBlockNodePos.ts
+++ src/client/src/components/flow/block/builders/helpers/SetBlockNodePos.ts
@@ -1,3 +1,5 @@
+import { Size } from "../../../../../compLibrary";
+
 /**
  * Function to force a node to fit within the parent block in BlockView.
  * @param nodePos
@@ -7,11 +9,21 @@
 const SetBlockNodePos = (nodePos: { x: number; y: number }, parentPos: { x: number; y: number }) => {
   const parentX = parentPos.x;
   const parentY = parentPos.y;
-  const nodeY = nodePos.y;
-  const dy = nodeY + parentY;
-  const dx = parentX + 100;
+  let nodeY = nodePos.y;
+  let nodeX = nodePos.x;
+  const xMargin = 20;
 
-  return { x: dx, y: dy };
+  const xMin = parentX;
+  const xMax = parentX + Size.BlockView_Width;
+  const yMin = parentY;
+  const yMax = parentY + Size.BlockView_Height;
+
+  if (nodeX < xMin) nodeX = xMin + xMargin;
+  if (nodeX > xMax) nodeX = xMax - (xMargin + Size.Node_Width);
+  if (nodeY < yMin) nodeY = yMin + 50;
+  if (nodeY > yMax) nodeY = yMax - 130;
+
+  return { x: nodeX, y: nodeY };
 };
 
 export default SetBlockNodePos;
