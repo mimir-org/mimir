06d7c359885dd67c9d53dd6ee1defa11b23566f3
Helge Mikael <helge.landro@bouvet.no>
2021-10-31 18:22:07 +0100

    Update yPos calculation

--- src/client/src/components/flow/block/nodes/blockNode/helpers/SetNodeLength.tsx
+++ src/client/src/components/flow/block/nodes/blockNode/helpers/SetNodeLength.tsx
@@ -6,11 +6,10 @@ import { IsInputTerminal } from "../../../../helpers";
  * Component to resize a Node based on the amount of visual terminals.
  * @param terminals
  * @param node
- * @returns the new value for Node length
  */
 const SetNodeLength = (terminals: Connector[], node: Node) => {
   const maximum = 5;
-  const increaseY = 20;
+  const increaseY = 25;
   let inTerminals = 0;
   let outTerminals = 0;
 
@@ -19,8 +18,9 @@ const SetNodeLength = (terminals: Connector[], node: Node) => {
     t.visible && !IsInputTerminal(t) && outTerminals++;
   });
 
-  if (inTerminals > maximum) node.length = inTerminals * increaseY;
-  else if (outTerminals > maximum) node.length = outTerminals * increaseY;
+  if (inTerminals > maximum) node.length = node.length + inTerminals * increaseY;
+  if (outTerminals > maximum) node.length = node.length + outTerminals * increaseY;
+  if (inTerminals === 0 && outTerminals === 0) node.length = Size.Node_Length;
   node.width = Size.Node_Width;
 };
 
--- src/client/src/components/flow/block/terminals/helpers/SetTerminalXPos.tsx
+++ src/client/src/components/flow/block/terminals/helpers/SetTerminalXPos.tsx
@@ -1,7 +1,7 @@
 import { Size } from "../../../../../compLibrary";
 
 /**
- * Method to calculate a terminal's X position in BlockView.
+ * Method to calculate a terminal's X position in BlockView. Terminals are positioned middle-out.
  * @param count the count of input or output terminals for a node.
  * @param parent
  * @param nodeWidth
@@ -11,13 +11,11 @@ const SetTerminalXPos = (count: number, parent: boolean, nodeWith: number) => {
   const interval = parent ? 35 : Size.Terminals_Interval; // Default horizontal distance between each terminal
   const base = nodeWith / 2 - 8; // Middle position
 
-  if (count === 0 || count === 1) return base;
-
   // Even-numbered terminals ordered left
   if (count % 2 === 0) return base - interval * (count / 2);
 
   // Odd-numbered terminals ordered right
-  if (count % 2 !== 0) return base + interval * Math.floor(count / 2);
+  if (count % 2 !== 0) return base + interval * Math.floor(count / 2) + interval;
 };
 
 export default SetTerminalXPos;
