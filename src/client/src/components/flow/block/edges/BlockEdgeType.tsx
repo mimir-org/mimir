b083cc182d0e248244325766c965c79bc533da5c
Helge Mikael <helge.landro@bouvet.no>
2021-11-04 13:52:17 +0100

    Fix edge

--- src/client/src/components/flow/block/edges/BlockEdgeType.tsx
+++ src/client/src/components/flow/block/edges/BlockEdgeType.tsx
@@ -1,8 +1,6 @@
 import { ArrowHeadType, getBezierPath, getMarkerEnd, getSmoothStepPath } from "react-flow-renderer";
-import { Color } from "../../../../compLibrary";
 import { Connector } from "../../../../models";
 import { IsLocationTerminal, IsProductTerminal } from "../../helpers";
-import { GetTerminalColor } from "../terminals/helpers";
 import { SetClassName, GetStyle } from "./helpers";
 
 /**
@@ -11,12 +9,10 @@ import { SetClassName, GetStyle } from "./helpers";
  * @returns a connection line between to Nodes.
  */
 export default function BlockEdgeType({ id, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, data }) {
-  const markerEnd = getMarkerEnd(ArrowHeadType.Arrow, null);
+  const markerEnd = getMarkerEnd(ArrowHeadType.ArrowClosed, null);
   const sourceConn = data.source.connectors?.find((conn: Connector) => conn.id === data.edge.fromConnectorId) as Connector;
   const hasLocation = IsLocationTerminal(sourceConn);
   const hasProduct = IsProductTerminal(sourceConn);
-  const color = GetTerminalColor(sourceConn);
-  const marginX = 3;
 
   const smoothPath = getSmoothStepPath({
     sourceX,
@@ -39,10 +35,7 @@ export default function BlockEdgeType({ id, sourceX, sourceY, targetX, targetY, 
   return (
     <>
       {!hasLocation && !hasProduct ? (
-        <>
-          <path id={id} style={GetStyle(sourceConn)} className="react-flow__edge-path" d={smoothPath} />
-          <circle cx={targetX - marginX} cy={targetY} fill={Color.White} r={3} stroke={color} strokeWidth={1.5} />
-        </>
+        <path id={id} style={GetStyle(sourceConn)} className="react-flow__edge-path" d={smoothPath} markerEnd={getMarkerEnd()} />
       ) : (
         <>
           <path id={id} style={GetStyle(sourceConn)} className={SetClassName(data) + ""} d={bezierPath} markerEnd={markerEnd} />
