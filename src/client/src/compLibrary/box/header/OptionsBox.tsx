d9772256c14a18d9d7eeec02736ea05aff0a3bd1
Helge Mikael <helge.landro@bouvet.no>
2021-11-04 14:31:36 +0100

    Fix header logos and position

--- src/client/src/compLibrary/box/header/OptionsBox.tsx
+++ src/client/src/compLibrary/box/header/OptionsBox.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 const OptionsBox = styled.div`
-  position: relative;
-  right: 18em;
-  top: 40px;
+  width: auto;
+  height: 40px;
 `;
 
 export default OptionsBox;
