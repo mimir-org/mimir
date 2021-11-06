726f68eda6471f54d1a092cc6f47d0ee4032cf23
Helge Mikael <helge.landro@bouvet.no>
2021-11-06 20:47:25 +0100

    Add styled component to OffPage

--- src/client/src/components/flow/styled/NodeBox.ts
+++ src/client/src/components/flow/styled/NodeBox.ts
@@ -23,7 +23,7 @@ const NodeBox = styled.div<Props>`
   border-color: ${(props) => (props.isSelected ? props.colorSelected : props.colorMain)};
   box-shadow: ${(props) => (!props.isSelected ? "0 5px 5px -2px rgba(0, 0, 0, 0.2)" : "none")};
   transition: border 250ms ease-in-out, box-shadow 250ms ease-in-out, height 250ms ease-in-out, width 250ms ease-in-out;
-  z-index: 2 !important;
+  z-index: 3 !important;
 
   &:hover {
     border-color: ${(props) => props.colorSelected} !important;
