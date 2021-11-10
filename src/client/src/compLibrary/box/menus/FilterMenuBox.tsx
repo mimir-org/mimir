3e48d7ed4149a01600954deef7253ab9194f3fda
Helge Mikael <helge.landro@bouvet.no>
2021-11-10 15:02:41 +0100

    Refactor

--- src/client/src/compLibrary/box/menus/FilterMenuBox.tsx
+++ src/client/src/compLibrary/box/menus/FilterMenuBox.tsx
@@ -15,8 +15,9 @@ const FilterMenuBox = styled.div<Props>`
   min-height: 140px;
   height: auto;
   border-style: solid;
-  border-color: ${Color.Grey};
+  border-color: ${Color.BlueMagenta};
   border-width: 0px 0px 1px 1px;
+  border-bottom-left-radius: 10px;
   z-index: 5;
   box-shadow: 0 5px 5px -2px rgba(0, 0, 0, 0.2);
   transition: right 0.2s ease-in-out;
