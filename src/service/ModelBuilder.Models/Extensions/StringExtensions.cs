73c2299de7212ac83afac76849242b1e4b92b3d6
Reidar Liabø <RLIAB@equinor.com>
2021-11-15 18:04:32 +0100

    Removed unused reference, bugfix position z in render cube

--- src/service/ModelBuilder.Models/Extensions/StringExtensions.cs
+++ src/service/ModelBuilder.Models/Extensions/StringExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Security.Cryptography;
 using System.Text;
 using Mb.Models.Application.TypeEditor;
