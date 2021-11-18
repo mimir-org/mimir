f8c6902fb6dee72a9f079753ce21e52a81a7e5d5
Helge Mikael <helge.landro@bouvet.no>
2021-11-18 16:36:48 +0100

    hotfix: resolve naming issues

--- src/client/src/assets/icons/header/index.ts
+++ src/client/src/assets/icons/header/index.ts
@@ -6,7 +6,7 @@ export { default as BlockViewActive } from "./blockViewActive.svg";
 export { default as Filter } from "./filter.svg";
 export { default as Horizontal } from "./horizontal.svg";
 export { default as Vertical } from "./vertical.svg";
-export { default as DarkMode } from "./darkMode.svg";
+export { default as DarkMode } from "./darkmode.svg";
 export { default as LightMode } from "./lightMode.svg";
 export { default as AvatarBackground } from "./avatarBackground.svg";
 export { default as Settings } from "./settings.svg";
