155f893c8d682737c3bfd2793fe3638ab8e361ba
Helge Mikael <helge.landro@bouvet.no>
2021-11-21 13:10:50 +0100

    Refactor

--- src/client/src/components/flow/tree/helpers/SetTreeNodePosition.ts
+++ src/client/src/components/flow/tree/helpers/SetTreeNodePosition.ts
@@ -5,16 +5,11 @@ const SetTreeNodePosition = (node: Node, parentNode: Node) => {
   const defaultAspectPosY = 100;
   if (IsAspectNode(node)) return { x: node.positionX, y: defaultAspectPosY };
 
-  return { x: calculateX(node, parentNode), y: calculateY(node) };
+  return { x: calculateX(node), y: calculateY(node) };
 };
 
-function calculateX(node: Node, parentNode: Node) {
-  let test = node?.positionX - parentNode.positionX / 2;
-
-  // if (test < parentNode.positionX || test > parentNode.positionX) test = parentNode.positionX;
-
-  // node.positionX = node?.positionX - test + parentNode?.positionX;
-  return test;
+function calculateX(node: Node) {
+  return node?.positionX;
 }
 
 function calculateY(node: Node) {
