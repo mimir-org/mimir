74aa688080741a18b22f368ae63dc849c04859be
Helge Mikael <helge.landro@bouvet.no>
2021-11-21 15:07:31 +0100

    Refactor Header component

---
+++ src/client/src/components/header/ProjectMenu.tsx
@@ -0,0 +1,28 @@
+import { CollapseWhiteIcon, ExpandWhiteIcon } from "../../assets/icons/chevron";
+import { TextResources } from "../../assets/text";
+import { OnProject } from "./handlers";
+import { ProjectBox } from "./styled";
+import { Project } from "../../models";
+
+interface Props {
+  projectMenuOpen: boolean;
+  project: Project;
+  dispatch: any;
+}
+/**
+ * Component for the ProjectMenu element in the header of Mimir.
+ * @param interface
+ * @returns a clickable element that toggles the ProjectMenuComponent.
+ */
+const ProjectMenu = ({ projectMenuOpen, project, dispatch }: Props) => (
+  <ProjectBox isOpen={projectMenuOpen} onClick={() => OnProject(dispatch, projectMenuOpen)}>
+    <p className="project-name">{project?.name ?? TextResources.Project}</p>
+    <img
+      src={projectMenuOpen ? CollapseWhiteIcon : ExpandWhiteIcon}
+      alt="icon"
+      className="toggle-icon"
+      onClick={() => OnProject(dispatch, projectMenuOpen)}
+    />
+  </ProjectBox>
+);
+export default ProjectMenu;
--- src/client/src/components/header/index.ts
+++ src/client/src/components/header/index.ts
@@ -1,2 +1,4 @@
 export { default as ToolBar } from "./ToolBar";
 export { default as Header } from "./Header";
+export { default as Avatar } from "./Avatar";
+export { default as ProjectMenu } from "./ProjectMenu";
