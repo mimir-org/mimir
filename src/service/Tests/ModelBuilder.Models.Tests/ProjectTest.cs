1ddf2c1f46631582fd8e53229151c0aac6b62ad5
Reidar Liabø <RLIAB@equinor.com>
2021-11-23 15:16:14 +0100

    Added extra id split tests

--- src/service/Tests/ModelBuilder.Models.Tests/ProjectTest.cs
+++ src/service/Tests/ModelBuilder.Models.Tests/ProjectTest.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Mb.Models.Application;
+using Mb.Models.Const;
 using Mb.Models.Data;
 using Mb.Models.Extensions;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,9 +12,10 @@ namespace ModelBuilder.Models.Tests
     public class ProjectTest : WebTestFixture<TestStartup>
     {
         [Theory]
-        [InlineData(null, @"https://rdf.equinor.com/sor/mimir/ID0b805fa8-1daa-4871-ac51-832607d52bbe", "equinor.com")]
-        public void Iri_Domain_Generates_Correct_Id(string id, string iri, string domain)
+        [InlineData(null, "equinor.com")]
+        public void Iri_Domain_Generates_Correct_Id(string id, string domain)
         {
+            var iri = $"{GlobalSettings.IriMimirPrefix}0b805fa8-1daa-4871-ac51-832607d52bbe";
             var mapper = ServiceProvider.GetService<IMapper>();
             Assert.NotNull(mapper);
 
@@ -51,7 +53,7 @@ namespace ModelBuilder.Models.Tests
         }
 
         [Theory]
-        [InlineData("invalidDomain_0b805fa8-1daa-4871-ac51-832607d52bbe", "invalidDomain", "invalidDomain")]
+        [InlineData("invalidDomain_0b805fa8-1daa-4871-ac51-832607d52bbe", "invalidIri", "invalidDomain")]
         public void All_Props_Should_Be_Written(string id, string iri, string domain)
         {
             var mapper = ServiceProvider.GetService<IMapper>();
@@ -68,6 +70,23 @@ namespace ModelBuilder.Models.Tests
             Assert.Equal(domain, project.Domain);
             Assert.True(project.Id.HasValidIri(project.Iri));
             Assert.Equal(id, project.Id);
+        }
+
+        [Theory]
+        [InlineData("invalidDomain_0b805fa8-1daa-4871-id51-832607d52bbe")]
+        public void Has_Valid_Iri_Correct_With_Id_As_Guid(string id)
+        {
+            var iri = $"{GlobalSettings.IriMimirPrefix}0b805fa8-1daa-4871-id51-832607d52bbe";
+            Assert.True(id.HasValidIri(iri));
+        }
+
+        [Theory]
+        [InlineData("invalidDomain_0b805fa8-1daa-4871-id51-832607d52bbe", "invalidDomain")]
+        public void Resolve_Id_Resolve_Correct(string id, string domain)
+        {
+            var iri = $"{GlobalSettings.IriMimirPrefix}0b805fa8-1daa-4871-id51-832607d52bbe";
+            var generatedId = iri.ResolveIdFromIriAndDomain(domain);
+            Assert.Equal(id, generatedId);
         }
     }
 }
