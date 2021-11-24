de553bfa214c613339bd403360b30cac826ccf44
Helge Mikael <helge.landro@bouvet.no>
2021-11-24 23:20:12 +0100

    Add aspect validation for tree edges

--- src/client/src/components/flow/block/terminals/helpers/IsValidBlockConnection.ts
+++ src/client/src/components/flow/block/terminals/helpers/IsValidBlockConnection.ts
@@ -4,14 +4,14 @@ import { Connector, Node } from "../../../../../models";
 import { setValidation } from "../../../../../redux/store/validation/actions";
 
 /**
- * Function to check if a connection/edge is valid.
+ * Function to check if a connection/edge in BlockView is valid.
  * @param conn
  * @param nodes
  * @param terminals
  * @param dispatch
  * @returns a boolean value.
  */
-const IsValidConnection = (conn: Connection, nodes: Node[], terminals: Connector[], dispatch) => {
+const IsValidBlockConnection = (conn: Connection, nodes: Node[], terminals: Connector[], dispatch) => {
   const targetNode = nodes.find((x) => x.id === conn.target);
   const sourceTerminal = terminals.find((x) => x.id === conn.sourceHandle);
   const targetTerminal = targetNode?.connectors.find((x) => x.id === conn.targetHandle);
@@ -22,4 +22,4 @@ const IsValidConnection = (conn: Connection, nodes: Node[], terminals: Connector
   return isValid;
 };
 
-export default IsValidConnection;
+export default IsValidBlockConnection;
