d96ae8a74ace95cd70aa38fb0608c9a28a3cc65d
Henrik Knudsen <henrik.knudsen@bouvet.no>
2021-12-03 10:16:53 +0100

    Fix setActive logic in relations tab for blockview.

--- src/client/src/modules/inspector/tabs/relations/handlers/OnClickNode.ts
+++ src/client/src/modules/inspector/tabs/relations/handlers/OnClickNode.ts
@@ -1,12 +1,13 @@
 import { MODULE_TYPE } from "../../../../../models/project";
 import { setModuleVisibility } from "../../../../../redux/store/modules/actions";
-import { setActiveEdge, setActiveNode } from "../../../../../redux/store/project/actions";
+import { setActiveBlockNode, setActiveEdge, setActiveNode } from "../../../../../redux/store/project/actions";
 import { changeInspectorTab } from "../../../redux/tabs/actions";
 import { Node } from "../../../../../models";
+import { IsBlockView } from "../../../../../helpers";
 
 export const OnClickNode = (node: Node, setActiveFlowElement: (elementId: string) => void, dispatch: any) => {
   dispatch(setActiveEdge(null, false));
-  dispatch(setActiveNode(node.id, true));
+  IsBlockView() ? dispatch(setActiveBlockNode(node.id)) : dispatch(setActiveNode(node.id, true));
   dispatch(setModuleVisibility(MODULE_TYPE.INSPECTOR, true, true));
   dispatch(changeInspectorTab(3));
   setActiveFlowElement(node.id);
--- src/client/src/modules/inspector/tabs/relations/handlers/OnClickTransport.ts
+++ src/client/src/modules/inspector/tabs/relations/handlers/OnClickTransport.ts
@@ -1,11 +1,12 @@
 import { Edge } from "../../../../../models";
 import { changeInspectorTab } from "../../../redux/tabs/actions";
-import { setActiveEdge, setActiveNode } from "../../../../../redux/store/project/actions";
+import { setActiveBlockNode, setActiveEdge, setActiveNode } from "../../../../../redux/store/project/actions";
 import { Dispatch } from "redux";
+import { IsBlockView } from "../../../../../helpers";
 
 const OnClickTransport = (edge: Edge, setActiveFlowElement: (elementId: string) => void, dispatch: Dispatch) => {
   dispatch(setActiveEdge(edge.id, true));
-  dispatch(setActiveNode(null, false));
+  IsBlockView() ? dispatch(setActiveBlockNode(null)) : dispatch(setActiveNode(null, false));
   dispatch(changeInspectorTab(3));
   setActiveFlowElement(edge.id);
 };
