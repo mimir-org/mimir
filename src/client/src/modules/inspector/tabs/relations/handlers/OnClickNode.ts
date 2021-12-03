97e29d903c3f0aaf97443a618a379474872bf268
Henrik Knudsen <henrik.knudsen@bouvet.no>
2021-12-03 10:26:00 +0100

    Refactor. Do not need setActive logic in relations tab anymore, handled by Flow via setActiveNodeElement and setActiveEdgeElement.

--- src/client/src/modules/inspector/tabs/relations/handlers/OnClickNode.ts
+++ src/client/src/modules/inspector/tabs/relations/handlers/OnClickNode.ts
@@ -1,14 +1,5 @@
-import { MODULE_TYPE } from "../../../../../models/project";
-import { setModuleVisibility } from "../../../../../redux/store/modules/actions";
-import { setActiveBlockNode, setActiveEdge, setActiveNode } from "../../../../../redux/store/project/actions";
-import { changeInspectorTab } from "../../../redux/tabs/actions";
 import { Node } from "../../../../../models";
-import { IsBlockView } from "../../../../../helpers";
 
-export const OnClickNode = (node: Node, setActiveFlowElement: (elementId: string) => void, dispatch: any) => {
-  dispatch(setActiveEdge(null, false));
-  IsBlockView() ? dispatch(setActiveBlockNode(node.id)) : dispatch(setActiveNode(node.id, true));
-  dispatch(setModuleVisibility(MODULE_TYPE.INSPECTOR, true, true));
-  dispatch(changeInspectorTab(3));
+export const OnClickNode = (node: Node, setActiveFlowElement: (elementId: string) => void) => {
   setActiveFlowElement(node.id);
 };
--- src/client/src/modules/inspector/tabs/relations/handlers/OnClickTransport.ts
+++ src/client/src/modules/inspector/tabs/relations/handlers/OnClickTransport.ts
@@ -1,13 +1,6 @@
 import { Edge } from "../../../../../models";
-import { changeInspectorTab } from "../../../redux/tabs/actions";
-import { setActiveBlockNode, setActiveEdge, setActiveNode } from "../../../../../redux/store/project/actions";
-import { Dispatch } from "redux";
-import { IsBlockView } from "../../../../../helpers";
 
-const OnClickTransport = (edge: Edge, setActiveFlowElement: (elementId: string) => void, dispatch: Dispatch) => {
-  dispatch(setActiveEdge(edge.id, true));
-  IsBlockView() ? dispatch(setActiveBlockNode(null)) : dispatch(setActiveNode(null, false));
-  dispatch(changeInspectorTab(3));
+const OnClickTransport = (edge: Edge, setActiveFlowElement: (elementId: string) => void) => {
   setActiveFlowElement(edge.id);
 };
 
