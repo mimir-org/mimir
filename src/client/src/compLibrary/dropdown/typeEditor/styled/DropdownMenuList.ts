3f47a3eb1c1617464cb7c70ae4f8bc1dca33f6c9
Helge Mikael <helge.landro@bouvet.no>
2021-12-06 20:06:23 +0100

    Dropdown adjust

--- src/client/src/compLibrary/dropdown/typeEditor/styled/DropdownMenuList.ts
+++ src/client/src/compLibrary/dropdown/typeEditor/styled/DropdownMenuList.ts
@@ -10,7 +10,7 @@ const DropdownMenuList = styled.div`
   border-radius: 5px;
   background-color: inherit;
   position: absolute;
-  top: 50px;
+  top: 52px;
   left: 0;
   z-index: 1;
   width: 99%;
