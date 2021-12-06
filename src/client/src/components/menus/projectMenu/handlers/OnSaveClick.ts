d29c511be75e35f66d7ee8a9fba84e6a03c1c2ae
Helge Mikael <helge.landro@bouvet.no>
2021-12-06 15:22:06 +0100

    Remove offpage stuff on save

--- src/client/src/components/menus/projectMenu/handlers/OnSaveClick.ts
+++ src/client/src/components/menus/projectMenu/handlers/OnSaveClick.ts
@@ -1,10 +1,23 @@
 import { ProjectState } from "../../../../redux/store/project/types";
 import { setProjectMenuVisibility } from "../../../menus/projectMenu/subMenus/redux/actions";
-import { save } from "../../../../redux/store/project/actions";
+import { removeEdge, removeNode, save } from "../../../../redux/store/project/actions";
+import { IsOffPage } from "../../../../helpers";
 
 const OnSaveClick = (dispatch: any, projectState: ProjectState) => {
   dispatch(setProjectMenuVisibility(false));
-  if (projectState.project) dispatch(save(projectState.project));
+
+  // Clear all OffPage elements
+  if (projectState.project) {
+    projectState.project.nodes.forEach((node) => {
+      if (IsOffPage(node)) dispatch(removeNode(node.id));
+    });
+
+    projectState.project.edges.forEach((edge) => {
+      if (IsOffPage(edge.fromNode) || IsOffPage(edge.toNode)) dispatch(removeEdge(edge.id));
+    });
+
+    dispatch(save(projectState.project));
+  }
 };
 
 export default OnSaveClick;
