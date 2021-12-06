d94fe996763d5dcaa1dd97c8ddec64d2075bff30
Olav Gjerde <fruithut@tutanota.com>
2021-12-06 20:37:06 +0100

    Adds global box-sizing: border-box

--- src/client/src/compLibrary/index.ts
+++ src/client/src/compLibrary/index.ts
@@ -10,4 +10,4 @@ export { default as DropdownList } from "./dropdown/DropdownList";
 export { default as DropdownHeader } from "./dropdown/DropdownHeader";
 export { default as DirectionalDropdown } from "./dropdown/DirectionalDropdown";
 
-export { default as GlobalStyle } from "./globalStyles";
+export { default as GlobalStyle } from "./GlobalStyle";
