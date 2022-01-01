37734f5b6a486ec5d8ddb922f5dd04057d580aa1
Helge Mikael <helge.landro@bouvet.no>
2022-01-01 16:38:14 +0100

    Update ValidateBlockEdge for OffPage

--- src/client/src/components/flow/validators/ValidateBlockEdge.tsx
+++ src/client/src/components/flow/validators/ValidateBlockEdge.tsx
@@ -20,10 +20,16 @@ const ValidateBlockEdge = (
   source: Connector,
   target: Connector
 ) => {
-  if (IsOffPage(fromNode)) return validateOffPageSourceEdge(toNode, selectedNode, source, target);
-  if (IsOffPage(toNode)) return validateOffPageTargetEdge(fromNode, selectedNode, source, target);
-  if (!secondaryNode) return validateEdge(selectedNode, fromNode, toNode, source, target);
-  if (secondaryNode) return validateSecondaryEdge(selectedNode, secondaryNode, fromNode, source, target);
+  if (!secondaryNode) {
+    if (IsOffPage(fromNode)) return validateOffPageSourceEdge(toNode, selectedNode, source, target);
+    if (IsOffPage(toNode)) return validateOffPageTargetEdge(fromNode, toNode, selectedNode, source, target);
+    return validateEdge(selectedNode, fromNode, toNode, source, target);
+  }
+  if (secondaryNode) {
+    if (IsOffPage(toNode)) return !toNode.isConnectedOffPage;
+    if (IsOffPage(fromNode)) return !fromNode.isConnectedOffPage;
+    return validateSecondaryEdge(selectedNode, secondaryNode, fromNode, source, target);
+  }
 };
 
 function validateEdge(selectedNode: Node, fromNode: Node, toNode: Node, source: Connector, target: Connector) {
@@ -32,9 +38,10 @@ function validateEdge(selectedNode: Node, fromNode: Node, toNode: Node, source: 
     return true;
   }
 
-  if (!IsDirectChild(fromNode, selectedNode)) return false;
+  if (!IsDirectChild(fromNode, selectedNode) || !IsDirectChild(toNode, selectedNode)) return false;
   if (IsProduct(selectedNode)) return (IsTransportConnection(source, target) || IsPartOf(source)) && IsProduct(fromNode);
   if (IsLocation(selectedNode)) return IsLocationConnection(source, target) && IsLocation(fromNode);
+
   return IsTransportConnection(source, target) && IsFunction(fromNode);
 }
 
@@ -52,8 +59,8 @@ function validateSecondaryEdge(selectedNode: Node, secondaryNode: Node, fromNode
 function validateOffPageSourceEdge(toNode: Node, selectedNode: Node, source: Connector, target: Connector) {
   return IsTransportConnection(source, target) && IsDirectChild(toNode, selectedNode);
 }
-function validateOffPageTargetEdge(fromNode: Node, selectedNode: Node, source: Connector, target: Connector) {
-  return IsTransportConnection(source, target) && IsDirectChild(fromNode, selectedNode);
+function validateOffPageTargetEdge(fromNode: Node, toNode: Node, selectedNode: Node, source: Connector, target: Connector) {
+  return IsTransportConnection(source, target) && IsDirectChild(fromNode, selectedNode) && IsDirectChild(toNode, fromNode);
 }
 
 export default ValidateBlockEdge;
--- src/client/src/helpers/IsDirectChild.ts
+++ src/client/src/helpers/IsDirectChild.ts
@@ -2,7 +2,7 @@ import { GetParent } from "../components/flow/helpers";
 import { Node } from "../models";
 
 const IsDirectChild = (child: Node, parent: Node) => {
-  return child?.level - parent?.level === 1 && GetParent(child).id === parent.id;
+  return GetParent(child)?.id === parent?.id;
 };
 
 export default IsDirectChild;
