b1add031652a341e6d401816ff7c78dffe414af7
Olav Gjerde <fruithut@tutanota.com>
2022-01-17 16:07:40 +0100

    Hotfix: Corrects filtering of nodes/edges in VisualFilterComponent

--- src/client/src/components/menus/filterMenu/helpers/GetFilterElements.ts
+++ src/client/src/components/menus/filterMenu/helpers/GetFilterElements.ts
@@ -1,27 +1,23 @@
 import { Elements } from "react-flow-renderer";
 import { IsOffPage } from "../../../../helpers";
-import { EDGE_KIND, Edge, Node } from "../../../../models";
-import { EDGE_TYPE } from "../../../../models/project";
+import { Edge, Node } from "../../../../models";
 
-const GetFilterElements = (elements: Elements<Node | Edge>) => {
+const GetFilterElements = (elements: Elements): { nodes: Node[], edges: Edge[] } => {
   const nodes: Node[] = [];
   const edges: Edge[] = [];
 
-  const edgeTypes = Object.values(EDGE_TYPE);
-
   elements?.forEach((elem) => {
-    const isEdge = edgeTypes.some((x) => x === elem.type?.toString() || elem.data.kind === EDGE_KIND);
+    const edge = elem.data?.edge as Edge;
+    const node = elem.data as Node;
 
-    if (isEdge) {
-      const edge = elem.data as Edge;
-      if (edge && !IsOffPage(edge.fromNode)) edges.push(edge);
-    } else {
-      const node = elem.data as Node;
-      if (node && !IsOffPage(node)) nodes.push(node);
+    if (edge && !IsOffPage(edge.fromNode)) {
+      edges.push(edge);
+    } else if (node && !IsOffPage(node)) {
+      nodes.push(node);
     }
   });
 
-  return [nodes, edges];
+  return { nodes, edges };
 };
 
 export default GetFilterElements;
