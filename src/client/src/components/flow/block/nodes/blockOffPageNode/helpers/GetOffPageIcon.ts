e692af153491ea456a8a24a557633a2746ac52e8
Helge Mikael <helge.landro@bouvet.no>
2022-02-23 13:03:05 +0100

    Restructure flow components

--- src/client/src/components/flow/block/nodes/blockOffPageNode/helpers/GetOffPageIcon.ts
+++ src/client/src/components/flow/block/nodes/blockOffPageNode/helpers/GetOffPageIcon.ts
@@ -5,11 +5,11 @@ import { IsInputTerminal } from "../../../../helpers";
 const GetOffPageIcon = (offPageterminal: Connector, sourceTerminal: Connector) => {
   if (IsInputTerminal(offPageterminal)) {
     if (sourceTerminal?.isRequired) return icons.OffPageRequiredInputIcon;
-    else return icons.OffPageConnectedInputIcon;
+    return icons.OffPageConnectedInputIcon;
   }
 
   if (sourceTerminal?.isRequired) return icons.OffPageRequiredOutputIcon;
-  else return icons.OffPageConnectedOutputIcon;
+  return icons.OffPageConnectedOutputIcon;
 };
 
 export default GetOffPageIcon;
--- src/client/src/components/message/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MessageComponent } from "./MessageComponent";
