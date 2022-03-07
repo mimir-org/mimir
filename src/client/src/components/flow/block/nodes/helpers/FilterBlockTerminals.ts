14d0124ca4084b83143be5bdd50672484d7616f8
Helge Mikael <helge.landro@bouvet.no>
2022-03-07 20:07:09 +0100

    Refactor and rename FilterBlockTerminals

--- src/client/src/components/flow/block/nodes/helpers/FilterBlockTerminals.ts
+++ src/client/src/components/flow/block/nodes/helpers/FilterBlockTerminals.ts
@@ -9,25 +9,24 @@ import { IsLocationTerminal, IsPartOf, IsProductTerminal, IsTransport } from "..
  * @param secondaryNode
  * @returns a filtered list of connectors sorted by type and name.
  */
-export const FilterTerminals = (actualNode: Node, secondaryNode: Node) => {
+export const FilterBlockTerminals = (actualNode: Node, secondaryNode: Node) => {
   const selectedNode = GetSelectedNode();
 
   return actualNode?.connectors
-    ?.filter((c) => !IsPartOf(c) && ValidateTerminal(selectedNode, secondaryNode, c))
+    ?.filter((c) => !IsPartOf(c) && FilterTerminal(selectedNode, secondaryNode, c))
     ?.sort((a, b) => a.type - b.type || a.name.localeCompare(b.name));
 };
 
-function ValidateTerminal(selectedNode: Node, secondary: Node, c: Connector) {
-  if (secondary) return ValidateSplitViewTerminal(selectedNode, secondary, c);
+function FilterTerminal(selectedNode: Node, secondary: Node, c: Connector) {
+  if (secondary) return FilterSplitViewTerminal(selectedNode, secondary, c);
   if (IsLocation(selectedNode)) return IsLocationTerminal(c);
   return IsTransport(c);
 }
 
-function ValidateSplitViewTerminal(selectedNode: Node, secondary: Node, c: Connector) {
+function FilterSplitViewTerminal(selectedNode: Node, secondary: Node, c: Connector) {
   if (!IsLocation(selectedNode)) {
     if (IsFunction(secondary)) return IsTransport(c);
     if (IsProduct(secondary)) return IsProductTerminal(c);
-    if (IsLocation(secondary)) return IsLocationTerminal(c);
   }
   return IsLocationTerminal(c);
 }
