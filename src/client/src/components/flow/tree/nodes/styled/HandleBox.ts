29766fa82d769a52676cc2da8c4503da54d6173f
Helge Mikael <helge.landro@bouvet.no>
2022-03-08 14:44:50 +0100

    Center transport/relation connectors in TreeView

--- src/client/src/components/flow/tree/nodes/styled/HandleBox.ts
+++ src/client/src/components/flow/tree/nodes/styled/HandleBox.ts
@@ -18,8 +18,8 @@ export const HandleBox = styled.div<Props>`
     border-radius: 0;
     bottom: -18px;
     z-index: 1;
-    right: ${(props) => props.position === Position.Right && 3}px;
-    left: ${(props) => props.position === Position.Left && 3}px;
+    right: ${(props) => props.position === Position.Right && 50}%;
+    left: ${(props) => props.position === Position.Left && 50}%;
     top: ${(props) => props.topPos};
     transition: opacity 0.5s ease-in-out;
   }
