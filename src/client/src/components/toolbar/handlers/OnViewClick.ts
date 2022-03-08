c4eb85d1b7935ef3bee74477041df79fec4b0947
Helge Mikael <helge.landro@bouvet.no>
2022-03-08 09:07:33 +0100

    Add validation for BlockView click on multiple selected nodes

---
+++ src/client/src/components/toolbar/handlers/OnViewClick.ts
@@ -0,0 +1,37 @@
+import { VIEW_TYPE, ViewType } from "../../../models/project";
+import { changeFlowView } from "../../../redux/store/flow/flowSlice";
+import { removeSecondaryNode } from "../../../redux/store/secondaryNode/actions";
+import { GetSelectedNode } from "../../../helpers";
+import { updateBlockElements } from "../../../modules/explorer/redux/actions";
+import { setValidation } from "../../../redux/store/validation/validationSlice";
+import { TextResources } from "../../../assets/text";
+import { Dispatch } from "redux";
+import { Node } from "../../../models";
+
+const OnViewClick = (view: ViewType, numberOfSelectedElements: number, dispatch: Dispatch) => {
+  const selectedNode = GetSelectedNode();
+
+  if (!ValidateBlockViewClick(view, selectedNode, numberOfSelectedElements, dispatch)) return;
+
+  dispatch(removeSecondaryNode());
+  dispatch(updateBlockElements([]));
+  dispatch(changeFlowView(view));
+};
+
+export default OnViewClick;
+
+function ValidateBlockViewClick(view: ViewType, selectedNode: Node, numberOfSelectedElements: number, dispatch: Dispatch) {
+  // BlockView can only be opened when a node is selected
+  if (view === VIEW_TYPE.BLOCKVIEW && !selectedNode) {
+    dispatch(setValidation({ valid: false, message: TextResources.Validation_BlockView }));
+    return false;
+  }
+
+  // BlockView can not be opened if multiple nodes are selected
+  if (numberOfSelectedElements > 1) {
+    dispatch(setValidation({ valid: false, message: TextResources.Validation_BlockView_Multiselect }));
+    return false;
+  }
+
+  return true;
+}
