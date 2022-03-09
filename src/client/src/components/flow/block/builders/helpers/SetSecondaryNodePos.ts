84a3c380ffd69ca5673a842603db405b8c0310b6
Helge Mikael <helge.landro@bouvet.no>
2022-03-09 19:20:58 +0100

    Update nodePos and secondaryNodePos

--- src/client/src/components/flow/block/builders/helpers/SetSecondaryNodePos.ts
+++ src/client/src/components/flow/block/builders/helpers/SetSecondaryNodePos.ts
@@ -7,21 +7,22 @@ import { Position } from "../../../../../models/project";
  * @returns an updated position, containing X and Y values.
  */
 const SetSecondaryNodePos = (nodePos: Position) => {
-  const margin = 20;
   const width = window.innerWidth / Size.BLOCK_SPLITVIEW_DIVISOR;
+  const margin = 30;
+  const marginLarge = 100;
 
-  const yMin = 30;
-  const yMax = window.innerHeight - 180;
-  const xMin = width + 300;
+  const xMin = width + marginLarge * 2;
   const xMax = width * 2.1;
+  const yMin = margin;
+  const yMax = window.innerHeight - marginLarge * 2;
 
   let nodeY = nodePos.y;
   let nodeX = nodePos.x;
 
-  if (nodeX < xMin) nodeX = xMin;
-  if (nodeX > xMax) nodeX = xMax;
+  if (nodeX < xMin) nodeX = xMin + margin;
+  if (nodeX > xMax) nodeX = xMax - marginLarge;
   if (nodeY < yMin) nodeY = yMin + margin;
-  if (nodeY > yMax) nodeY = yMax - margin * 4.5;
+  if (nodeY > yMax) nodeY = yMax - margin * 2;
 
   return { x: nodeX, y: nodeY };
 };
