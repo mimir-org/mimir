4297eada705ecb4162944bbbb2976547cf2bf277
Helge Mikael <helge.landro@bouvet.no>
2022-03-09 17:42:35 +0100

    Refactor SetNodePos

--- src/client/src/components/flow/block/builders/helpers/SetNodePos.ts
+++ src/client/src/components/flow/block/builders/helpers/SetNodePos.ts
@@ -12,20 +12,20 @@ import { Position } from "../../../../../models/project";
 const SetNodePos = (nodePos: Position, splitView: boolean, isProduct: boolean) => {
   const width = splitView ? window.innerWidth / Size.BLOCK_SPLITVIEW_DIVISOR : window.innerWidth - Size.BLOCK_MARGIN_X;
   const margin = 30;
-  const marginLarge = 80;
+  const marginLarge = 100;
 
-  const yMin = margin;
-  const yMax = window.innerHeight - 180;
   const xMin = marginLarge;
   const xMax = isProduct ? Size.BLOCK_PRODUCT_WIDTH : width - margin;
+  const yMin = margin;
+  const yMax = window.innerHeight - marginLarge * 2;
 
   let nodeX = nodePos.x;
   let nodeY = nodePos.y;
 
   if (nodeX < xMin) nodeX = xMin + margin;
-  if (nodeX > xMax) nodeX = xMax - margin;
-  if (nodeY < yMin) nodeY = yMin + 20;
-  if (nodeY > yMax) nodeY = yMax - margin * 3.5;
+  if (nodeX > xMax) nodeX = xMax - margin * 2;
+  if (nodeY < yMin) nodeY = yMin + margin;
+  if (nodeY > yMax) nodeY = yMax - marginLarge;
 
   return { x: nodeX, y: nodeY };
 };
