13bdddf2885b93d8fd9c9b11c753744961688de3
Helge Mikael <helge.landro@bouvet.no>
2022-03-22 14:14:49 +0100

    Refactor Block builders and helpers

--- src/client/src/components/flow/block/builders/BuildFlowSecondaryChildNode.ts
+++ src/client/src/components/flow/block/builders/BuildFlowSecondaryChildNode.ts
@@ -10,14 +10,15 @@ import { BlockNodeSize } from "../../../../models/project";
  * This component creates a FlowElement that contains the basic data for a node.
  * On top of the FlowNode a layer with Mimir functionality is created. See the BlockNode component.
  * @param node
+ * @param size
  * @returns a node that sits inside the container of the ParentSecondaryNode.
  */
-const BuildFlowSecondaryChildNode = (node: Node, parentNodeSize: BlockNodeSize) => {
+const BuildFlowSecondaryChildNode = (node: Node, size: BlockNodeSize) => {
   if (!node) return null;
 
   const type = GetNodeTypeString(node);
   const nodePos = { x: node.positionBlockX, y: node.positionBlockY };
-  const position = !IsOffPage(node) ? SetChildNodePosition(nodePos, parentNodeSize, true) : nodePos;
+  const position = !IsOffPage(node) ? SetChildNodePosition(nodePos, size, true) : nodePos;
 
   return {
     key: CreateId(),
--- src/client/src/components/flow/block/builders/helpers/DrawFlowSecondaryChildren.ts
+++ src/client/src/components/flow/block/builders/helpers/DrawFlowSecondaryChildren.ts
@@ -7,22 +7,24 @@ import { BlockNodeSize } from "../../../../../models/project";
 /**
  * Component to draw all secondaryNode children nodes in BlockView.
  * @param project
- * @param secondary
+ * @param secondaryNode
  * @param elements
- * @param parentNodeSize
+ * @param size
  */
-const DrawFlowSecondaryChildren = (project: Project, secondary: Node, elements: Elements, parentNodeSize: BlockNodeSize) => {
+const DrawFlowSecondaryChildren = (project: Project, secondaryNode: Node, elements: Elements, size: BlockNodeSize) => {
   const nodes = project.nodes;
   const edges = project.edges;
 
-  if (secondary) {
-    edges?.forEach((edge) => {
-      if (edge.fromNodeId === secondary.id && IsFamily(secondary, edge.toNode)) {
-        const targetNode = nodes.find((n) => n.id === edge.toNodeId && IsDirectChild(n, secondary));
-        if (targetNode) elements.push(BuildFlowSecondaryChildNode(targetNode, parentNodeSize));
-      }
-    });
-  }
+  edges?.forEach((edge) => {
+    const isChild = edge.fromNodeId === secondaryNode.id && IsFamily(secondaryNode, edge.toNode);
+    if (!isChild) return;
+
+    const targetNode = nodes.find((n) => n.id === edge.toNodeId && IsDirectChild(n, secondaryNode));
+    if (!targetNode) return;
+
+    const childNode = BuildFlowSecondaryChildNode(targetNode, size);
+    if (childNode) elements.push(childNode);
+  });
 };
 
 export default DrawFlowSecondaryChildren;
