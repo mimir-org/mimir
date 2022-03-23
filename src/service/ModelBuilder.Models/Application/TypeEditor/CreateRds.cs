87af9c8070804aa9eeb91a6d859b71bf53fb99de
Kai Andre Hovden <kai.hovden@bouvet.no>
2022-03-23 15:04:02 +0100

    Added/renamed objects to RDS.json

--- src/service/ModelBuilder.Models/Application/TypeEditor/CreateRds.cs
+++ src/service/ModelBuilder.Models/Application/TypeEditor/CreateRds.cs
@@ -8,6 +8,6 @@ namespace Mb.Models.Application.TypeEditor
         public string Code { get; set; }
         public string Name { get; set; }
         public string Iri { get; set; }
-        [JsonIgnore] public string Key => $"{Code}-{Name}";
+        [JsonIgnore] public string Key => $"{Code}";
     }
 }
