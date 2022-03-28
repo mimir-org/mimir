12a95ca5a1c904a1f9e90d3036bde0e9381d8b36
Helge Mikael <helge.landro@bouvet.no>
2022-03-29 00:48:51 +0200

    Update boundaries for SplitView OffPageNodes

--- src/client/src/components/flow/block/builders/helpers/SetOffPageNodePos.ts
+++ src/client/src/components/flow/block/builders/helpers/SetOffPageNodePos.ts
@@ -4,25 +4,23 @@ import { Position } from "../../../../../models/project";
 import { GetParent } from "../../../helpers";
 
 /**
- * Component to force an OffPageNode to fit into the position of the ParentNode.
+ * Component to force an OffPageNode to fit the position of the ParentNode.
  * @param offPageNode
  * @param parentNode
- * @returns an updated position, containing X and Y values.
+ * @returns a Position object.
  */
 const SetOffPageNodePos = (offPageNode: Node, parentNode: Node, secondaryNode: Node) => {
   if (!offPageNode || !parentNode) return null;
 
   // Handle OffPageNodes from the SecondaryNode
   if (secondaryNode !== undefined) {
-    const splitOffPagePos = HandleSplitViewOffPage(parentNode, secondaryNode, offPageNode);
+    const splitOffPagePos = HandleSplitViewOffPage(secondaryNode, offPageNode);
     if (splitOffPagePos !== null) return splitOffPagePos;
   }
 
-  // If the OffPageNode is a targetNode find the position to the right of the parentNode
-  if (offPageNode.isOffPageTarget) return HandleTargetOffPage(parentNode, offPageNode);
+  if (offPageNode.isOffPageTarget) return HandleTargetOffPagePos(parentNode, offPageNode);
 
-  // If the OffPageNode is a sourceNode find the position to the left of the parentNode
-  return HandleSourceOffPage(parentNode, offPageNode);
+  return HandleSourceOffPagePos(parentNode, offPageNode);
 };
 
 /**
@@ -31,7 +29,7 @@ const SetOffPageNodePos = (offPageNode: Node, parentNode: Node, secondaryNode: N
  * @param offPageNode
  * @returns a Position object.
  */
-function HandleSourceOffPage(parentNode: Node, offPageNode: Node) {
+function HandleSourceOffPagePos(parentNode: Node, offPageNode: Node) {
   const marginX = 35;
   const marginY = 30;
 
@@ -53,7 +51,7 @@ function HandleSourceOffPage(parentNode: Node, offPageNode: Node) {
  * @param offPageNode
  * @returns a Position object.
  */
-function HandleTargetOffPage(parentNode: Node, offPageNode: Node) {
+function HandleTargetOffPagePos(parentNode: Node, offPageNode: Node) {
   const marginX = parentNode.width !== Size.BLOCK_NODE_WIDTH ? 30 : 0;
   const marginY = 30;
 
@@ -71,34 +69,18 @@ function HandleTargetOffPage(parentNode: Node, offPageNode: Node) {
 
 /**
  * Function to force the position of an OffPageNode that is a child of the SecondaryNode.
- * @param parentNode
  * @param secondaryNode
  * @param offPageNode
  * @returns a Position object.
  */
-function HandleSplitViewOffPage(parentNode: Node, secondaryNode: Node, offPageNode: Node) {
-  let x = 0;
-  let y = 0;
-
+function HandleSplitViewOffPage(secondaryNode: Node, offPageNode: Node) {
   const offPageParent = GetParent(offPageNode);
   const parentBlock = GetParent(offPageParent);
 
   if (parentBlock?.id !== secondaryNode?.id) return null;
-  const leftBound = parentNode.positionBlockX + parentNode.width + Size.SPLITVIEW_DISTANCE;
 
-  // OffPage is source
-  if (offPageNode.positionBlockX <= leftBound) {
-    const margin = 35;
-    x = leftBound - margin;
-    y = offPageNode.positionBlockY;
-    return { x, y };
-  }
-
-  // OffPage is target
-  x = secondaryNode.positionBlockX + secondaryNode.width;
-  y = offPageNode.positionBlockY;
-
-  return { x, y } as Position;
+  if (offPageNode.isOffPageTarget) return HandleTargetOffPagePos(parentBlock, offPageNode);
+  return HandleSourceOffPagePos(parentBlock, offPageNode);
 }
 
 export default SetOffPageNodePos;
--- src/client/src/components/flow/block/builders/helpers/index.ts
+++ src/client/src/components/flow/block/builders/helpers/index.ts
@@ -4,9 +4,7 @@ export { default as DrawFlowBlockEdges } from "./DrawFlowBlockEdges";
 
 export { default as SetChildNodePos } from "./SetChildNodePos";
 export { default as SetSecondaryChildNodePos } from "./SetSecondaryChildNodePos";
-
 export { default as SetOffPageNodePos } from "./SetOffPageNodePos";
-export { default as SetSecondaryOffPageNodePos } from "./SetSecondaryOffPageNodePos";
 
 export { default as GetNodeTypeString } from "./GetNodeTypeString";
 export { default as ValidateBlockEdge } from "./ValidateBlockEdge";
