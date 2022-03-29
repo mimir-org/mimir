b5bbe37708ff998d54e73b7ba873f081859dc05d
Olav Gjerde <fruithut@tutanota.com>
2022-03-29 13:17:19 +0200

    Adjusts criteria for displaying rds list in type editor

--- src/client/src/typeEditor/helpers/GetRDSList.ts
+++ src/client/src/typeEditor/helpers/GetRDSList.ts
@@ -1,16 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { CreateLibraryType, Aspect, ObjectType, Rds } from "../../models";
+import { CreateLibraryType, Aspect, Rds } from "../../models";
 
 const GetRDSList = (createLibraryType: CreateLibraryType, items: Rds[]): any[] => {
-  if (
-    createLibraryType.aspect !== Aspect.NotSet &&
-    createLibraryType.objectType !== ObjectType.NotSet &&
-    createLibraryType.purpose !== "" &&
-    createLibraryType.name !== "" &&
-    createLibraryType.symbolId !== ""
-  )
+  if (createLibraryType.aspect !== Aspect.NotSet && createLibraryType.aspect !== Aspect.None) {
     return items;
-  else return [];
+  }
+
+  return [];
 };
 
 export default GetRDSList;
