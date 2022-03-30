a68621d80f40f4fcb7f159f16ccc0da12ceb2955
Helge Mikael <helge.landro@bouvet.no>
2022-03-30 08:59:20 +0200

    Trigger rebuild on project change

--- src/client/src/components/flow/tree/FlowTree.tsx
+++ src/client/src/components/flow/tree/FlowTree.tsx
@@ -96,10 +96,10 @@ const FlowTree = ({ project, inspectorRef }: Props) => {
     }
   }, [project]);
 
-  // Build elements with new nodes
+  // Update elements
   useEffect(() => {
     if (project) setElements(BuildTreeElements(project, animatedEdge));
-  }, [project?.nodes?.length]);
+  }, [project]);
 
   useEffect(() => {
     project?.edges?.forEach((edge) => {
