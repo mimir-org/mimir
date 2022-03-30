9080c1c3f32e4191a93422f698100c3efd901147
Olav Gjerde <fruithut@tutanota.com>
2022-03-30 13:58:41 +0200

    Adjusts sorted variable naming in GetSharedCategory.ts

--- src/client/src/modules/library/components/body/components/collections/components/nodeCollection/helpers/GetSharedCategory.ts
+++ src/client/src/modules/library/components/body/components/collections/components/nodeCollection/helpers/GetSharedCategory.ts
@@ -3,10 +3,10 @@ import { LibraryCategory } from "../../../../../../../../../models/project";
 import { TextResources } from "../../../../../../../../../assets/text/TextResources";
 
 export const getSharedCategory = (items: LibItem[]): LibraryCategory => {
-  const sortedByAspect = [...items].sort((a, b) => a.aspect - b.aspect || a.name.localeCompare(b.name));
+  const sortedByAspectThenByName = [...items].sort((a, b) => a.aspect - b.aspect || a.name.localeCompare(b.name));
 
   return {
     name: TextResources.LIBRARY_CATEGORY_ALL,
-    nodes: sortedByAspect,
+    nodes: sortedByAspectThenByName,
   };
 };
