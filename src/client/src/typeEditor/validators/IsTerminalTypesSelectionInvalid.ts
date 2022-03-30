71fc5b8cee426500486830a11226cef57ccd918f
Olav Gjerde <fruithut@tutanota.com>
2022-03-30 15:51:02 +0200

    Corrects TerminalTypeSelection validation

--- src/client/src/typeEditor/validators/IsTerminalTypesSelectionInvalid.ts
+++ src/client/src/typeEditor/validators/IsTerminalTypesSelectionInvalid.ts
@@ -4,7 +4,7 @@ import { IsLocation } from "../helpers";
 export function IsTerminalTypesSelectionInvalid(createLibraryType: CreateLibraryType): boolean {
   const hasNoTerminals = createLibraryType.terminalTypes.length === 0;
   const hasTerminalsWithoutTypeId = createLibraryType.terminalTypes.some((t) => t.terminalTypeId === "");
-  const hasSingleTerminalIdWithoutTypes = createLibraryType.terminalTypeId && hasNoTerminals;
+  const hasSingleTerminalIdWithoutTypes = createLibraryType.terminalTypeId !== "" && hasNoTerminals;
 
   return (
     !IsLocation(createLibraryType.aspect) && (hasNoTerminals || hasTerminalsWithoutTypeId || hasSingleTerminalIdWithoutTypes)
