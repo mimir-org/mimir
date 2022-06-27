b9f6efd701fac859767eb3430567f29deef91f08
Helge Mikael <helge.landro@bouvet.no>
2022-06-27 15:34:35 +0200

    Update converters for LibraryDrop

--- src/client/src/components/flow/converters/ConvertTerminalLibCmToConnector.ts
+++ src/client/src/components/flow/converters/ConvertTerminalLibCmToConnector.ts
@@ -1,7 +1,8 @@
 import { CreateId } from "../helpers";
 import { ConvertAttributeLibCmToAttribute } from ".";
 import { ConnectorDirection, NodeTerminalLibCm } from "@mimirorg/typelibrary-types";
-import { Connector, ConnectorVisibility, Terminal } from "@mimirorg/modelbuilder-types";
+import { Connector, ConnectorVisibility, Relation, RelationType, Terminal } from "@mimirorg/modelbuilder-types";
+import { TextResources } from "../../../assets/text/TextResources";
 
 const ConvertTerminalLibCmToConnector = (nodeTerminals: NodeTerminalLibCm[], nodeId: string) => {
   const connectors = [] as Connector[];
@@ -11,11 +12,47 @@ const ConvertTerminalLibCmToConnector = (nodeTerminals: NodeTerminalLibCm[], nod
     for (let i = 0; i < t.quantity; i++) connectors.push(connector);
   });
 
+  // Add all Relations
+  connectors.push(CreateRelation(nodeId, RelationType.PartOf, TextResources.PARTOF_RELATIONSHIP, ConnectorDirection.Input));
+  connectors.push(CreateRelation(nodeId, RelationType.PartOf, TextResources.PARTOF_RELATIONSHIP, ConnectorDirection.Output));
+  connectors.push(CreateRelation(nodeId, RelationType.HasLocation, TextResources.HAS_LOCATION, ConnectorDirection.Input));
+  connectors.push(CreateRelation(nodeId, RelationType.HasLocation, TextResources.HAS_LOCATION, ConnectorDirection.Output));
+  connectors.push(CreateRelation(nodeId, RelationType.FulfilledBy, TextResources.FULFILLED_BY, ConnectorDirection.Input));
+  connectors.push(CreateRelation(nodeId, RelationType.FulfilledBy, TextResources.FULFILLED_BY, ConnectorDirection.Output));
+
   return connectors;
 };
 
 export default ConvertTerminalLibCmToConnector;
 
+/**
+ * Function to add a Relation to a Node.
+ * @param nodeId
+ * @param relationType
+ * @param connectorDirection
+ * @returns a Relation
+ */
+function CreateRelation(nodeId: string, relationType: RelationType, name: string, connectorDirection: ConnectorDirection) {
+  return {
+    id: CreateId(),
+    type: connectorDirection,
+    nodeId,
+    relationType,
+    kind: "Connector",
+    discriminator: "Relation", // TODO: fix?
+    name: name,
+    domain: "runir.net",
+    isRequired: false,
+    connectorVisibility: ConnectorVisibility.None,
+  } as Relation;
+}
+
+/**
+ * Function to create a Terminal based on the NodeTerminalLibCm type.
+ * @param item
+ * @param nodeId
+ * @returns a Terminal
+ */
 function CreateTerminal(item: NodeTerminalLibCm, nodeId: string) {
   const connectorVisibility = SetConnectorVisibility(item.connectorDirection);
   const attributes = ConvertAttributeLibCmToAttribute(item.terminal.attributes);
