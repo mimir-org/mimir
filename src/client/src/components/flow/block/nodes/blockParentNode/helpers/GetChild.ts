c49420c60642cade7d6f4616b051ce96a45b2b4b
Helge Mikael <helge.landro@bouvet.no>
2022-09-01 00:44:26 +0200

    Update documentation and resolve todo tag

--- src/client/src/components/flow/block/nodes/blockParentNode/helpers/GetChild.ts
+++ src/client/src/components/flow/block/nodes/blockParentNode/helpers/GetChild.ts
@@ -2,11 +2,12 @@ import red from "../../../../../../redux/store/index";
 import { IsPartOfRelation } from "../../../../helpers/Connectors";
 
 /**
- * Function to find a node's childNode
+ * Function to find a node's childNode.
+ * NOTE: this is used to traverse down one step in BlockView. How to find the correct child node is not yet implemented.
  * @param nodeId - the parent node
- * @returns the parentNode
+ * @returns the childNode's id
  */
-// TODO: this is used to traverse down one step in BlockView. How to find the correct child node will be defined later.
+
 export const GetChild = (nodeId: string) => {
   const edges = red.store.getState().projectState.project.edges;
   const nodes = red.store.getState().projectState.project.nodes;
--- src/client/src/components/flow/block/nodes/blockParentNode/helpers/InitParentSize.ts
+++ src/client/src/components/flow/block/nodes/blockParentNode/helpers/InitParentSize.ts
@@ -5,17 +5,17 @@ import { updateBlockSize } from "../../../../../../redux/store/project/actions";
 
 /**
  * Component to ensure that the ParentNode size has been initialized.
- * Originaly the size is set when dropping a Node from the Library.
+ * Originally the size is set when dropping a Node from the Library.
  * This component serves as a fallback if the size has not been properly set.
  * @param node
  * @param dispatch
  */
 export const InitParentSize = (node: Node, dispatch: Dispatch) => {
   if (
-    node?.width === null ||
-    node?.height === null ||
-    node?.width === undefined ||
-    node?.height === undefined ||
+    node?.width == null ||
+    node?.height == null ||
+    node?.width == undefined ||
+    node?.height == undefined ||
     node?.width < Size.BLOCK_NODE_MIN_WIDTH ||
     node?.height < Size.BLOCK_NODE_MIN_HEIGHT
   ) {
