abea494e4cd90909adb675bfc322bd90a4c332d4
Reidar Liabø <RLIAB@equinor.com>
2022-09-02 19:40:02 +0200

    Testing new pre release

--- src/service/ModelBuilder.Rdf/Extensions/AttributeExtensions.cs
+++ src/service/ModelBuilder.Rdf/Extensions/AttributeExtensions.cs
@@ -204,7 +204,6 @@ namespace ModelBuilder.Rdf.Extensions
 
             attribute.SelectType = ontologyService.GetEnumValue<Select>(iri, Resources.SelectType, false);
             attribute.Discipline = ontologyService.GetEnumValue<Discipline>(iri, Resources.HasDiscipline, false);
-
         }
     }
 }
